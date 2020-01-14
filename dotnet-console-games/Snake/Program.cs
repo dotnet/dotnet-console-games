2bd76e8730e6b5b75fc0c0c2a880b0450b83394f
Zachary Patten <sevenix.zp@gmail.com>
2020-01-13 22:11:43 -0500

    Snake, Tic Tac Toe, Wumpus World

---
+++ dotnet-console-games/Snake/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    enum Direction { Up = 0, Down = 1, Left = 2, Right = 3, }
+    enum Tile { Open = 0, Snake, Food, }
+
+    static readonly char[] DirectionChars = { '^', 'v', '<', '>', };
+    static readonly TimeSpan sleep = TimeSpan.FromMilliseconds(70);
+
+    static void Main()
+    {
+        int width = Console.WindowWidth;
+        int height = Console.WindowHeight;
+        Random random = new Random();
+        Tile[,] map = new Tile[width, height];
+        Direction? direction = null;
+        Queue<(int X, int Y)> snake = new Queue<(int X, int Y)>();
+        (int X, int Y) snakePosition = (width / 2, height / 2);
+        bool closeRequested = false;
+
+        Console.CursorVisible = false;
+        Console.Clear();
+        snake.Enqueue(snakePosition);
+        map[snakePosition.X, snakePosition.Y] = Tile.Snake;
+        PositionFood();
+        Console.SetCursorPosition(snakePosition.X, snakePosition.Y);
+        Console.Write('@');
+
+        void PositionFood()
+        {
+            List<(int X, int Y)> possibleCoordinates = new List<(int X, int Y)>();
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (map[i, j] == Tile.Open)
+                    {
+                        possibleCoordinates.Add((i, j));
+                    }
+                }
+            }
+            int index = random.Next(possibleCoordinates.Count);
+            (int X, int Y) position = possibleCoordinates[index];
+            map[position.X, position.Y] = Tile.Food;
+            Console.SetCursorPosition(position.X, position.Y);
+            Console.Write('+');
+        }
+
+        void GetDirection()
+        {
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.UpArrow: direction = Direction.Up; break;
+                case ConsoleKey.DownArrow: direction = Direction.Down; break;
+                case ConsoleKey.LeftArrow: direction = Direction.Left; break;
+                case ConsoleKey.RightArrow: direction = Direction.Right; break;
+                case ConsoleKey.Escape: closeRequested = true; break;
+            }
+        }
+
+        while (!direction.HasValue && !closeRequested)
+        {
+            GetDirection();
+        }
+
+        while (!closeRequested)
+        {
+            if (Console.WindowWidth != width || Console.WindowHeight != height)
+            {
+                Console.Clear();
+                Console.Write("Console was resized. Snake game has ended.");
+                return;
+            }
+
+            switch (direction)
+            {
+                case Direction.Up: snakePosition.Y--; break;
+                case Direction.Down: snakePosition.Y++; break;
+                case Direction.Left: snakePosition.X--; break;
+                case Direction.Right: snakePosition.X++; break;
+            }
+
+            if (snakePosition.X < 0 || snakePosition.X >= width ||
+                snakePosition.Y < 0 || snakePosition.Y >= height ||
+                map[snakePosition.X, snakePosition.Y] == Tile.Snake)
+            {
+                Console.Clear();
+                Console.Write("Game Over. Score: " + (snake.Count - 1) + ".");
+                return;
+            }
+
+            Console.SetCursorPosition(snakePosition.X, snakePosition.Y);
+            Console.Write(DirectionChars[(int)direction]);
+            snake.Enqueue((snakePosition.X, snakePosition.Y));
+            if (map[snakePosition.X, snakePosition.Y] == Tile.Food)
+            {
+                PositionFood();
+            }
+            else
+            {
+                (int X, int Y) tail = snake.Dequeue();
+                map[tail.X, tail.Y] = Tile.Open;
+                Console.SetCursorPosition(tail.X, tail.Y);
+                Console.Write(' ');
+            }
+            map[snakePosition.X, snakePosition.Y] = Tile.Snake;
+
+            if (Console.KeyAvailable)
+            {
+                GetDirection();
+            }
+
+            System.Threading.Thread.Sleep(sleep);
+        }
+        Console.Clear();
+        Console.Write("Snake game was closed.");
+    }
+}
---
+++ dotnet-console-games/Tic Tac Toe/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        const char X = 'X';
+        const char O = 'O';
+        const char _ = ' ';
+
+        var random = new Random();
+        var gameOver = false;
+        var playerTurn = true;
+        var board = new char[3, 3]
+        {
+            { _, _, _, },
+            { _, _, _, },
+            { _, _, _, },
+        };
+
+        void RenderBoard()
+        {
+            Console.WriteLine();
+            Console.WriteLine(" " + board[0, 0] + "  |  " + board[0, 1] + "  | " + board[0, 2]);
+            Console.WriteLine("    |     |");
+            Console.WriteLine(" ---+-----+---");
+            Console.WriteLine("    |     |");
+            Console.WriteLine(" " + board[1, 0] + "  |  " + board[1, 1] + "  | " + board[1, 2]);
+            Console.WriteLine("    |     |");
+            Console.WriteLine(" ---+-----+---");
+            Console.WriteLine("    |     |");
+            Console.WriteLine(" " + board[2, 0] + "  |  " + board[2, 1] + "  | " + board[2, 2]);
+        }
+
+        void PlayerMove()
+        {
+            var (row, column) = (0, 0);
+        PlayerMove:
+            Console.Clear();
+            RenderBoard();
+            Console.WriteLine();
+            Console.WriteLine("Choose a valid position and press enter.");
+            Console.SetCursorPosition(column * 6 + 1, row * 4 + 1);
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.UpArrow: row = Math.Abs((row - 1) % 3); goto PlayerMove;
+                case ConsoleKey.DownArrow: row = Math.Abs((row + 1) % 3); goto PlayerMove;
+                case ConsoleKey.LeftArrow: column = Math.Abs((column - 1) % 3); goto PlayerMove;
+                case ConsoleKey.RightArrow: column = Math.Abs((column + 1) % 3); goto PlayerMove;
+                case ConsoleKey.Enter: break;
+                case ConsoleKey.Escape:
+                    Console.Clear();
+                    Console.Write("Tic Tac Toe was closed.");
+                    gameOver = true;
+                    break;
+                default: goto PlayerMove;
+            }
+            if (!gameOver)
+            {
+                if (board[row, column] != _)
+                {
+                    goto PlayerMove;
+                }
+                board[row, column] = X;
+            }
+        }
+
+        void ComputerMove()
+        {
+            var possibleMoves = new List<(int X, int Y)>();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == _)
+                    {
+                        possibleMoves.Add((i, j));
+                    }
+                }
+            }
+            int index = random.Next(0, possibleMoves.Count);
+            var (X, Y) = possibleMoves[index];
+            board[X, Y] = O;
+        }
+
+        void Check()
+        {
+            bool CheckForThree(char c) =>
+                board[0, 0] == c && board[1, 0] == c && board[2, 0] == c ||
+                board[0, 1] == c && board[1, 1] == c && board[2, 1] == c ||
+                board[0, 2] == c && board[1, 2] == c && board[2, 2] == c ||
+                board[0, 0] == c && board[0, 1] == c && board[0, 2] == c ||
+                board[1, 0] == c && board[1, 1] == c && board[1, 2] == c ||
+                board[2, 0] == c && board[2, 1] == c && board[2, 2] == c ||
+                board[0, 0] == c && board[1, 1] == c && board[2, 2] == c ||
+                board[2, 0] == c && board[1, 1] == c && board[0, 2] == c;
+
+            bool CheckForDraw() =>
+                board[0, 0] != _ && board[1, 0] != _ && board[2, 0] != _ &&
+                board[0, 1] != _ && board[1, 1] != _ && board[2, 1] != _ &&
+                board[0, 2] != _ && board[1, 2] != _ && board[2, 2] != _;
+
+            if (CheckForThree(X))
+            {
+                Console.Clear();
+                Console.Write("You Win.");
+                gameOver = true;
+            }
+            else if (CheckForThree(O))
+            {
+                Console.Clear();
+                Console.Write("You Lose.");
+                gameOver = true;
+            }
+            else if (CheckForDraw())
+            {
+                Console.Clear();
+                Console.Write("Draw.");
+                gameOver = true;
+            }
+        }
+
+        while (!gameOver)
+        {
+            if (playerTurn)
+            {
+                PlayerMove();
+            }
+            else
+            {
+                ComputerMove();
+            }
+            Check();
+            playerTurn = !playerTurn;
+        }
+    }
+}
