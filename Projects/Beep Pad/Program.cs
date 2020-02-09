63c5a30fef255547450d91b588e4e9032c038548
Zachary Patten <sevenix.zp@gmail.com>
2020-02-09 15:17:08 -0500

    wiping frequencies in Beep Pad

--- Projects/Beep Pad/Program.cs
+++ Projects/Beep Pad/Program.cs
@@ -14,15 +14,15 @@ class Program
 
 	static readonly int[] frequencies = new int[]
 	{
-		262,
-		294,
-		330,
-		349,
-		392,
-		440,
-		494,
-		523,
-		587,
+		100,
+		200,
+		300,
+		400,
+		500,
+		600,
+		700,
+		800,
+		900,
 	};
 
 	static void Main()
