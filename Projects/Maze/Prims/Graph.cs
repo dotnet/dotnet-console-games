8a7659f0555d82055e30d11c14aa27378ba33887
Kai <kai.jellinghaus1@gmail.com>
2020-02-29 17:26:35 +0100

    Add Maze Generator using Prims

---
+++ Projects/Maze/Prims/Graph.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class Graph
+{ 
+    public class Node
+    {
+        public int OwnIndex { get; }
+        public List<int> Connections { get; }
+        public List<double> Costs { get; }
+
+        public void Add(int other, double cost)
+        {
+            Connections.Add(other);
+            Costs.Add(cost);
+        }
+
+        public Node(int ownIndex)
+        {
+            OwnIndex = ownIndex;
+            Connections = new List<int>();
+            Costs = new List<double>();
+        }
+    }
+
+    public Node[] Nodes { get; }
+
+    public Graph(Node[] nodes)
+    {
+        Nodes = nodes ?? throw new ArgumentNullException(nameof(nodes));
+    }
+}
