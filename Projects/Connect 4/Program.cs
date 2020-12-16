61c3a8e658442859f6fb71e53cb9cbc4142197ed
Zachary Patten <sevenix.zp@gmail.com>
2020-12-16 18:58:15 -0500

    Connect 4

---
+++ Projects/Connect 4/Program.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Linq;
+
+bool?[,] board = new bool?[7, 6];
+bool player1Turn;
+bool player1MovesFirst = true;
+bool? player1Wins = default;
+Random random = new Random();
+
+const int moveMinI = 5;
+const int moveJ = 2;
+
+try
+{
+	Console.CursorVisible = false;
+PlayAgain:
+	player1Turn = player1MovesFirst;
+	player1MovesFirst = !player1MovesFirst;
+	ResetBoard();
+	while (player1Wins is null)
+	{
+		(int I, int J) move = default;
+		if (player1Turn)
+		{
+			RenderBoard();
+			int i = 0;
+			Console.SetCursorPosition(moveMinI, moveJ);
+			Console.Write('v');
+		GetPlayerInput:
+			switch (Console.ReadKey(true).Key)
+			{
+				case ConsoleKey.LeftArrow:
+					Console.SetCursorPosition(i * 2 + moveMinI, moveJ);
+					Console.Write(' ');
+					i = Math.Max(0, i - 1);
+					Console.SetCursorPosition(i * 2 + moveMinI, moveJ);
+					Console.Write('v');
+					goto GetPlayerInput;
+				case ConsoleKey.RightArrow:
+					Console.SetCursorPosition(i * 2 + moveMinI, moveJ);
+					Console.Write(' ');
+					i = Math.Min(board.GetLength(0) - 1, i + 1);
+					Console.SetCursorPosition(i * 2 + moveMinI, moveJ);
+					Console.Write('v');
+					goto GetPlayerInput;
+				case ConsoleKey.Enter:
+					if (board[i, board.GetLength(1) - 1] != null)
+					{
+						goto GetPlayerInput;
+					}
+					for (int j = board.GetLength(1) - 1; ; j--)
+					{
+						if (j is 0 || board[i, j - 1].HasValue)
+						{
+							board[i, j] = true;
+							move = (i, j);
+							break;
+						}
+					}
+					break;
+				case ConsoleKey.Escape:
+					Console.Clear();
+					return;
+				default: goto GetPlayerInput;
+			}
+			if (CheckFor4(move.I, move.J))
+			{
+				RenderBoard();
+				Console.WriteLine();
+				Console.WriteLine("   You Win!");
+				goto PlayAgainCheck;
+			}
+		}
+		else
+		{
+			int[] moves = Enumerable.Range(0, board.GetLength(0)).Where(i => !board[i, board.GetLength(1) - 1].HasValue).ToArray();
+			int randomMove = moves[random.Next(moves.Length)];
+			for (int j = board.GetLength(1) - 1; ; j--)
+			{
+				if (j is 0 || board[randomMove, j - 1].HasValue)
+				{
+					board[randomMove, j] = false;
+					move = (randomMove, j);
+					break;
+				}
+			}
+			if (CheckFor4(move.I, move.J))
+			{
+				RenderBoard();
+				Console.WriteLine();
+				Console.WriteLine($"   You Lose!");
+				goto PlayAgainCheck;
+			}
+		}
+		if (CheckForDraw())
+		{
+			RenderBoard();
+			Console.WriteLine();
+			Console.WriteLine($"   Draw!");
+			goto PlayAgainCheck;
+		}
+		else
+		{
+			player1Turn = !player1Turn;
+		}
+	}
+PlayAgainCheck:
+	Console.WriteLine("   Play Again [enter], or quit [escape]?");
+GetInput:
+	switch (Console.ReadKey(true).Key)
+	{
+		case ConsoleKey.Enter: goto PlayAgain;
+		case ConsoleKey.Escape: Console.Clear(); return;
+		default: goto GetInput;
+	}
+}
+finally
+{
+	Console.CursorVisible = true;
+}
+
+void ResetBoard()
+{
+	for (int i = 0; i < board.GetLength(0); i++)
+	{
+		for (int j = 0; j < board.GetLength(1); j++)
+		{
+			board[i, j] = null;
+		}
+	}
+}
+
+void RenderBoard()
+{
+	Console.Clear();
+	Console.WriteLine();
+	Console.WriteLine();
+	Console.WriteLine();
+	Console.WriteLine("   ╔" + new string('-', board.GetLength(0) * 2 + 1) + "╗");
+	Console.Write("   ║ ");
+	int iOffset = Console.CursorLeft;
+	int jOffset = Console.CursorTop;
+	Console.WriteLine(new string(' ', board.GetLength(0) * 2) + "║");
+	for (int j = 1; j < board.GetLength(1) * 2; j++)
+	{
+		Console.WriteLine("   ║" + new string(' ', board.GetLength(0) * 2 + 1) + "║");
+	}
+	Console.WriteLine("   ╚" + new string('═', board.GetLength(0) * 2 + 1) + "╝");
+	int iFinal = Console.CursorLeft;
+	int jFinal = Console.CursorTop;
+	for (int i = 0; i < board.GetLength(0); i++)
+	{
+		for (int j = 0; j < board.GetLength(1); j++)
+		{
+			Console.SetCursorPosition(i * 2 + iOffset, (board.GetLength(1) - j) * 2 + jOffset - 1);
+			if (board[i, j] == true)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.Write('█');
+				Console.ResetColor();
+			}
+			else if (board[i, j] == false)
+			{
+				Console.ForegroundColor = ConsoleColor.Blue;
+				Console.Write('█');
+				Console.ResetColor();
+			}
+			else
+			{
+				Console.ResetColor();
+				Console.Write(' ');
+			}
+		}
+	}
+	Console.SetCursorPosition(iFinal, jFinal);
+}
+
+bool CheckFor4(int i, int j)
+{
+	bool player = board[i, j].Value;
+	{ // horizontal
+		int inARow = 0;
+		for (int _i = 0; _i < board.GetLength(0); _i++)
+		{
+			inARow = board[_i, j] == player ? inARow + 1 : 0;
+			if (inARow >= 4) return true;
+		}
+	}
+	{ // vertical
+		int inARow = 0;
+		for (int _j = 0; _j < board.GetLength(1); _j++)
+		{
+			inARow = board[i, _j] == player ? inARow + 1 : 0;
+			if (inARow >= 4) return true;
+		}
+	}
+	{ // postive slope diagonal
+		int inARow = 0;
+		int min = Math.Min(i, j);
+		for (int _i = i - min, _j = j - min; _i < board.GetLength(0) && _j < board.GetLength(1); _i++, _j++)
+		{
+			inARow = board[_i, _j] == player ? inARow + 1 : 0;
+			if (inARow >= 4) return true;
+		}
+	}
+	{ // negative slope diagonal
+		int inARow = 0;
+		int offset = Math.Min(i, board.GetLength(1) - (j + 1));
+		for (int _i = i - offset, _j = j + offset; _i < board.GetLength(0) && _j >= 0; _i++, _j--)
+		{
+			inARow = board[_i, _j] == player ? inARow + 1 : 0;
+			if (inARow >= 4) return true;
+		}
+	}
+	return false;
+}
+
+bool CheckForDraw()
+{
+	for (int i = 0; i < board.GetLength(0); i++)
+	{
+		if (!board[i, board.GetLength(1) - 1].HasValue)
+		{
+			return false;
+		}
+	}
+	return true;
+}
---
+++ Projects/Connect 4/README.md
@@ -0,0 +1,20 @@
+<h1 align="center">
+	Connect 4
+</h1>
+
+<p align="center">
+	<a href="https://github.com/ZacharyPatten/dotnet-console-games" alt="GitHub repo"><img alt="flat" src="https://img.shields.io/badge/github-repo-black?logo=github&amp;style=flat"></a>
+	<a href="https://docs.microsoft.com/en-us/dotnet/csharp/" alt="GitHub repo"><img alt="Language C#" src="https://img.shields.io/badge/language-C%23-%23178600"></a>
+	<a href="https://dotnet.microsoft.com/download"><img src="https://img.shields.io/badge/dynamic/xml?color=%23512bd4&label=target&query=%2F%2FTargetFramework%5B1%5D&url=https%3A%2F%2Fraw.githubusercontent.com%2FZacharyPatten%2Fdotnet-console-games%2Fmaster%2FProjects%2FConnect%25204%2FConnect%25204.csproj&logo=.net" title="Target Framework" alt="Target Framework"></a>
+	<a href="https://github.com/ZacharyPatten/dotnet-console-games/actions"><img src="https://github.com/ZacharyPatten/dotnet-console-games/workflows/Connect%204%20Build/badge.svg" title="Goto Build" alt="Build"></a>
+	<a href="https://discord.gg/4XbQbwF" alt="Discord"><img src="https://img.shields.io/discord/557244925712924684?logo=discord&logoColor=ffffff&color=7389D8" title="Go To Discord Server" alt="Discord"/></a>
+	<a href="https://github.com/ZacharyPatten/dotnet-console-games/blob/master/LICENSE" alt="license"><img src="https://img.shields.io/badge/license-MIT-green.svg" /></a>
+</p>
+
+**[Source Code](Program.cs)**
+
+Connect 4 is a game where you try to get 4 pieces in a line before your oponent does on a 7x6 board by dropping them the pieces in from the top.
+
+## Input
+
+The **right and left arrow keys (←, →)** are used to move your drop location. The **enter key** will drop in your piece. The **escape** key may be used to close the game at any time.
