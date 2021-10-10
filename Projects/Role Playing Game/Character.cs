e336d827004f95f62bba46861e7fe79ea22f8a3c
Zachary Patten <sevenix.zp@gmail.com>
2021-10-09 20:20:25 -0400

    Role Playing Game initial

---
+++ Projects/Role Playing Game/Character.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Role_Playing_Game
+{
+	public class Character
+	{
+		public int Level { get; set; }
+		public int Experience { get; set; }
+		public int ExperienceToNextLevel { get; set; }
+		public int Health { get; set; }
+		public int MaxHealth { get; set; }
+		public int Gold { get; set; }
+		public int I { get; set; }
+		public int J { get; set; }
+		public int TileI => I < 0 ? (I - 6) / 7 : I / 7;
+		public int TileJ => J < 0 ? (J - 3) / 4 : J / 4;
+		private string[] _mapAnaimation;
+		public string[] MapAnimation
+		{
+			get => _mapAnaimation;
+			set
+			{
+				_mapAnaimation = value;
+				_mapAnimationFrame = 0;
+			}
+		}
+		private int _mapAnimationFrame;
+		public int MapAnimationFrame
+		{
+			get => _mapAnimationFrame;
+			set
+			{
+				_mapAnimationFrame = value;
+				Moved = false;
+				if (_mapAnimationFrame >= MapAnimation.Length)
+				{
+					if (MapAnimation == Sprites.RunUp)    { Moved = true; MapAnimation = Sprites.IdleUp;    }
+					if (MapAnimation == Sprites.RunDown)  { Moved = true; MapAnimation = Sprites.IdleDown;  }
+					if (MapAnimation == Sprites.RunLeft)  { Moved = true; MapAnimation = Sprites.IdleLeft;  }
+					if (MapAnimation == Sprites.RunRight) { Moved = true; MapAnimation = Sprites.IdleRight; }
+					_mapAnimationFrame = 0;
+				}
+			}
+		}
+		public bool IsIdle
+		{
+			get =>
+				_mapAnaimation == Sprites.IdleDown ||
+				_mapAnaimation == Sprites.IdleUp ||
+				_mapAnaimation == Sprites.IdleLeft ||
+				_mapAnaimation == Sprites.IdleRight;
+		}
+		public string Render =>
+			_mapAnaimation is not null && _mapAnimationFrame < _mapAnaimation.Length
+			? _mapAnaimation[_mapAnimationFrame]
+			: // "T" pose :D
+			  @" __O__ " + '\n' +
+			  @"   |   " + '\n' +
+			  @"   |   " + '\n' +
+			  @"  | |  ";
+		public bool Moved { get; set; } = false;
+	}
+}
