47358a2c809a96f3720f14e8189c4a4cffd2f760
Zachary Patten <sevenix.zp@gmail.com>
2022-02-10 21:30:56 -0500

    Quick Draw

---
+++ Projects/Quick Draw/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Diagnostics;
+
+Random random = new();
+
+const string menu = @"
+  Quick Draw
+
+  Face your opponent and wait for the signal. Once the
+  signal is given, shoot your opponent by pressing [space]
+  before they shoot you. It's all about your reaction time.
+
+  Choose Your Opponent:
+  [1] Easy....1000 milliseconds
+  [2] Medium...500 milliseconds
+  [3] Hard.....250 milliseconds
+  [4] Harder...125 milliseconds
+  [escape] give up";
+
+const string wait = @"
+  Quick Draw
+                                                        
+                                                        
+                                                        
+              _O                          O_            
+             |/|_          wait          _|\|           
+             /\                            /\           
+            /  |                          |  \          
+  ------------------------------------------------------";
+
+const string fire = @"
+  Quick Draw
+                                                        
+                         ********                       
+                         * FIRE *                       
+              _O         ********         O_            
+             |/|_                        _|\|           
+             /\          spacebar          /\           
+            /  |                          |  \          
+  ------------------------------------------------------";
+
+const string loseTooSlow = @"
+  Quick Draw
+                                                        
+                                                        
+                                                        
+                                        > ╗__O          
+           //            Too Slow           / \         
+          O/__/\         You Lose          /\           
+               \                          |  \          
+  ------------------------------------------------------";
+
+const string loseTooFast = @"
+  Quick Draw
+                                                        
+                                                        
+                                                        
+                         Too Fast       > ╗__O          
+           //           You Missed          / \         
+          O/__/\         You Lose          /\           
+               \                          |  \          
+  ------------------------------------------------------";
+
+const string win = @"
+  Quick Draw
+                                                        
+                                                        
+                                                        
+            O__╔ <                                      
+           / \                               \\         
+             /\          You Win          /\__\O        
+            /  |                          /             
+  ------------------------------------------------------";
+
+
+try
+{
+	while (true)
+	{
+		Console.Clear();
+		Console.WriteLine(menu);
+		TimeSpan? requiredReactionTime = null;
+		while (requiredReactionTime is null)
+		{
+			Console.CursorVisible = false;
+			switch (Console.ReadKey(true).Key)
+			{
+				case ConsoleKey.D1 or ConsoleKey.NumPad1: requiredReactionTime = TimeSpan.FromMilliseconds(1000); break;
+				case ConsoleKey.D2 or ConsoleKey.NumPad2: requiredReactionTime = TimeSpan.FromMilliseconds(0500); break;
+				case ConsoleKey.D3 or ConsoleKey.NumPad3: requiredReactionTime = TimeSpan.FromMilliseconds(0250); break;
+				case ConsoleKey.D4 or ConsoleKey.NumPad4: requiredReactionTime = TimeSpan.FromMilliseconds(0125); break;
+				case ConsoleKey.Escape: return;
+			}
+		}
+		Console.Clear();
+		TimeSpan signal = TimeSpan.FromMilliseconds(random.Next(5000, 35000));
+		Console.WriteLine(wait);
+		Stopwatch stopwatch = new();
+		stopwatch.Restart();
+		bool tooFast = false;
+		while (stopwatch.Elapsed < signal && !tooFast)
+		{
+			if (Console.KeyAvailable)
+			{
+				if (Console.ReadKey(true).Key is ConsoleKey.Spacebar)
+				{
+					tooFast = true;
+				}
+			}
+		}
+		Console.Clear();
+		Console.CursorVisible = false;
+		Console.WriteLine(fire);
+		stopwatch.Restart();
+		bool tooSlow = true;
+		TimeSpan reactionTime = default;
+		while (!tooFast && stopwatch.Elapsed < requiredReactionTime && tooSlow)
+		{
+			if (Console.KeyAvailable)
+			{
+				if (Console.ReadKey(true).Key is ConsoleKey.Spacebar)
+				{
+					tooSlow = false;
+					reactionTime = stopwatch.Elapsed;
+				}
+			}
+		}
+		Console.Clear();
+		Console.WriteLine(
+			tooFast ? loseTooFast :
+			tooSlow ? loseTooSlow :
+			$"{win}{Environment.NewLine}  Reaction Time: {reactionTime.TotalMilliseconds} milliseconds");
+		Console.WriteLine("  Play Again [enter] or quit [escape]?");
+		Console.CursorVisible = false;
+	GetEnterOrEscape:
+		switch (Console.ReadKey(true).Key)
+		{
+			case ConsoleKey.Enter: break;
+			case ConsoleKey.Escape: return;
+			default: goto GetEnterOrEscape;
+		}
+	}
+}
+finally
+{
+	Console.Clear();
+	Console.CursorVisible = true;
+	Console.Write("Quick Draw was closed.");
+}
---
+++ Projects/Quick Draw/README.md
@@ -0,0 +1,35 @@
+<h1 align="center">
+	Quick Draw
+</h1>
+
+<p align="center">
+	<a href="https://github.com/ZacharyPatten/dotnet-console-games" alt="GitHub repo"><img alt="flat" src="https://img.shields.io/badge/github-repo-black?logo=github&amp;style=flat"></a>
+	<a href="https://docs.microsoft.com/en-us/dotnet/csharp/" alt="GitHub repo"><img alt="Language C#" src="https://img.shields.io/badge/language-C%23-%23178600"></a>
+	<a href="https://dotnet.microsoft.com/download"><img src="https://img.shields.io/badge/dynamic/xml?color=%23512bd4&label=target&query=%2F%2FTargetFramework%5B1%5D&url=https%3A%2F%2Fraw.githubusercontent.com%2FZacharyPatten%2Fdotnet-console-games%2Fmaster%2FProjects%2FQuick%2520Draw%2FQuick%2520Draw.csproj&logo=.net" title="Target Framework" alt="Target Framework"></a>
+	<a href="https://github.com/ZacharyPatten/dotnet-console-games/actions"><img src="https://github.com/ZacharyPatten/dotnet-console-games/workflows/Quick%20Draw%20Build/badge.svg" title="Goto Build" alt="Build"></a>
+	<a href="https://discord.gg/4XbQbwF" alt="Discord"><img src="https://img.shields.io/discord/557244925712924684?logo=discord&logoColor=ffffff&color=7389D8" title="Go To Discord Server" alt="Discord"/></a>
+	<a href="https://github.com/ZacharyPatten/dotnet-console-games/blob/master/LICENSE" alt="license"><img src="https://img.shields.io/badge/license-MIT-green.svg" /></a>
+</p>
+
+**[Source Code](Program.cs)**
+
+Quick Draw is a game to test your reaction time. Face your opponent, wait for the signal, then fire your gun as fast as you can. React too slow and you will take a bullet. Attempt to fire too fast and you may miss your shot.
+
+```
+                                                        
+                                                        
+                                        > ╗__O          
+           //            Too Slow           / \         
+          O/__/\         You Lose          /\           
+               \                          |  \          
+  ------------------------------------------------------
+```
+
+## Input
+
+This game uses commands. Each screen will give you the list of available commands you may use at the time.
+
+- `1`, `2`, `3`, `4`: select your difficulty in the menu
+- `spacebar` : fire your gun
+- `enter`: play again
+- `escape`: close the game
