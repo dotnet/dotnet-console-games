a924dfbe91716e2d7b6e220134650727c81db6d7
Zachary Patten <sevenix.zp@gmail.com>
2022-02-11 21:23:10 -0500

    thanks scott harden :)

--- Blazor/Games/QuickDraw.cs
+++ Blazor/Games/QuickDraw.cs
@@ -1,10 +1,11 @@
 ﻿using System.Diagnostics;
+using Console = Blazor.Console<Blazor.Games.QuickDraw>;
 
-namespace Blazor;
+namespace Blazor.Games;
 
-public static class QuickDraw
+public class QuickDraw
 {
-	public static async Task Run()
+	public static async void Run()
 	{
 		Random random = new();
 
@@ -145,70 +146,6 @@ public static class QuickDraw
 			Console.Clear();
 			Console.CursorVisible = true;
 			Console.Write("Quick Draw was closed.");
-		}
-	}
-
-	public static class Console
-	{
-		const int delay = 1;
-
-		public static Action? refresh;
-
-		public static Queue<ConsoleKeyInfo> inputBuffer = new();
-
-		public static string state = string.Empty;
-
-		public static void Clear()
-		{
-			state = string.Empty;
-			refresh?.Invoke();
-		}
-
-		public static void Write(object o)
-		{
-			state += o.ToString();
-			refresh?.Invoke();
-		}
-
-		public static void WriteLine()
-		{
-			state += Environment.NewLine;
-			refresh?.Invoke();
-		}
-
-		public static void WriteLine(object o)
-		{
-			state += o.ToString() + Environment.NewLine;
-			refresh?.Invoke();
-		}
-
-		public static async Task<ConsoleKeyInfo> ReadKey(bool capture)
-		{
-			while (!await KeyAvailable())
-			{
-				await Task.Delay(delay);
-				refresh?.Invoke();
-			}
-			var keyInfo = inputBuffer.Dequeue();
-			if (!capture && keyInfo.KeyChar >= 32)
-			{
-				state += keyInfo.KeyChar;
-			}
-			return keyInfo;
-		}
-
-		public static bool _cursorVisible;
-
-		public static bool CursorVisible
-		{
-			get => _cursorVisible;
-			set => _cursorVisible = value;
-		}
-
-		public static async Task<bool> KeyAvailable()
-		{
-			await Task.Delay(delay);
-			return inputBuffer.Count > 0;
 		}
 	}
 }
