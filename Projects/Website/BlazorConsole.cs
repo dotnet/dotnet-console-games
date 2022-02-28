e1af962a7dae377c9ea999f9a930f4e160a50ca2
Zachary Patten <sevenix.zp@gmail.com>
2022-02-27 22:31:39 -0500

    Website BlazorOperatingSystem -> BlazorConsole cleanup

--- Projects/Website/BlazorConsole.cs
+++ Projects/Website/BlazorConsole.cs
@@ -426,4 +426,13 @@ public class BlazorConsole
 			continue;
 		}
 	}
+
+	/// <summary>
+	/// Returns true. Some members of <see cref="Console"/> only work
+	/// on Windows such as <see cref="Console.WindowWidth"/>, but even though this
+	/// is blazor and not necessarily on Windows, this wrapper contains implementations
+	/// for those Windows-only members.
+	/// </summary>
+	/// <returns>true</returns>
+	public bool IsWindows() => true;
 }
