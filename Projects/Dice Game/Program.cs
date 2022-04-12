0cbf091b2faef8f00e01fec33dec604d4b7175a0
Zachary Patten <sevenix.zp@gmail.com>
2022-04-12 19:55:49 -0400

    top level statements

--- Projects/Dice Game/Program.cs
+++ Projects/Dice Game/Program.cs
@@ -1,64 +1,58 @@
 ﻿using System;
 
-	class Program
+int playerRandomNum;
+int rivalRandomNum;
+
+int playerPoints = 0;
+int rivalPoints = 0;
+
+Random random = new Random();
+
+for (int i = 0; i < 10; i++)
+{
+	Console.WriteLine("Press any key to roll the dice:");
+
+	Console.ReadKey();
+
+	playerRandomNum = random.Next(1, 7);
+	Console.WriteLine("You rolled a " + playerRandomNum);
+
+	Console.WriteLine("....");
+	System.Threading.Thread.Sleep(1000);
+
+	rivalRandomNum = random.Next(1, 7);
+	Console.WriteLine("Rival rolled a " + rivalRandomNum);
+
+	if (playerRandomNum > rivalRandomNum)
 	{
-		static void Main(string[] args)
-		{
-			int playerRandomNum;
-			int rivalRandomNum;
-
-			int playerPoints = 0;
-			int rivalPoints = 0;
-
-			Random random = new Random();
-
-			for (int i = 0; i < 10; i++)
-			{
-				Console.WriteLine("Press any key to roll the dice:");
-
-				Console.ReadKey();
-
-				playerRandomNum = random.Next(1, 7);
-				Console.WriteLine("You rolled a " + playerRandomNum);
-
-				Console.WriteLine("....");
-				System.Threading.Thread.Sleep(1000);
-
-				rivalRandomNum = random.Next(1, 7);
-				Console.WriteLine("Rival rolled a " + rivalRandomNum);
-
-				if (playerRandomNum > rivalRandomNum)
-				{
-					playerPoints++;
-					Console.WriteLine("The Player has won this round.");
-				}
-				else if (playerRandomNum < rivalRandomNum)
-				{
-					rivalPoints++;
-					Console.WriteLine("The Rival has won this round.");
-				}
-				else
-				{
-					Console.WriteLine("This round is a draw!");
-				}
-
-				Console.WriteLine("The score is now - Player : " + playerPoints + ". Rival : " + rivalPoints + ".");
-				Console.WriteLine();
-			}
-
-			if (playerPoints > rivalPoints)
-			{
-				Console.WriteLine("You have won!");
-			}
-			else if (playerPoints < rivalPoints)
-			{
-				Console.WriteLine("You have lost!");
-			}
-			else
-			{
-				Console.WriteLine("This game is a draw.");
-			}
-
-			Console.ReadKey();
-		}
+		playerPoints++;
+		Console.WriteLine("The Player has won this round.");
 	}
+	else if (playerRandomNum < rivalRandomNum)
+	{
+		rivalPoints++;
+		Console.WriteLine("The Rival has won this round.");
+	}
+	else
+	{
+		Console.WriteLine("This round is a draw!");
+	}
+
+	Console.WriteLine("The score is now - Player : " + playerPoints + ". Rival : " + rivalPoints + ".");
+	Console.WriteLine();
+}
+
+if (playerPoints > rivalPoints)
+{
+	Console.WriteLine("You have won!");
+}
+else if (playerPoints < rivalPoints)
+{
+	Console.WriteLine("You have lost!");
+}
+else
+{
+	Console.WriteLine("This game is a draw.");
+}
+
+Console.ReadKey();
