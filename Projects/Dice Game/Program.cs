8b53f031093aaf566d9feaf00770630ac0b99c49
Zachary Patten <sevenix.zp@gmail.com>
2022-04-12 20:01:52 -0400

    add introductory prompt when game launches

--- Projects/Dice Game/Program.cs
+++ Projects/Dice Game/Program.cs
@@ -5,6 +5,17 @@ int rivalPoints = 0;
 
 Random random = new Random();
 
+Console.WriteLine("Dice Game");
+Console.WriteLine();
+Console.WriteLine("In this game you and a computer Rival will play 10 rounds");
+Console.WriteLine("where you will each roll a 6-sided dice, and the player");
+Console.WriteLine("with the highest dice value will win the round. The player");
+Console.WriteLine("who wins the most rounds wins the game. Good luck!");
+Console.WriteLine();
+Console.Write("Press any key to start...");
+Console.ReadKey(true);
+Console.WriteLine();
+Console.WriteLine();
 for (int i = 0; i < 10; i++)
 {
 	Console.WriteLine("Press any key to roll the dice:");
