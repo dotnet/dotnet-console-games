9f65bb5cdfff54edc8333fbbdc82945ff7b88b13
dernewt <dernewt@gmail.com>
2022-05-30 21:52:24 -0400

    clean up unused code

--- Projects/Rock Paper Scissors/Program.cs
+++ Projects/Rock Paper Scissors/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Rock_Paper_Scissors;
 
-//var game = new PaperScissorsRocks(new RandomPlayer(), new RandomPlayer());
 var game = new PaperScissorsRocks(new HumanPlayer(), new RandomPlayer());
 
 var stillPlaying = true;
