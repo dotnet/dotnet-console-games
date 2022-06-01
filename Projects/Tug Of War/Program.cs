d6158678cd19b0b60ea70043891291cd21d92257
Zachary Patten <sevenix.zp@gmail.com>
2022-06-01 18:01:56 -0400

    tug of war updates

--- Projects/Tug Of War/Program.cs
+++ Projects/Tug Of War/Program.cs
@@ -1,104 +1,153 @@
 ﻿using System;
 using System.Threading;
 
-Restart:
-int position = 0;
-int displacement = 10;
-bool frame_a = false;
-Console.Clear();
-Console.WriteLine(@"
+try
+{
+	while (true)
+	{
+		int position = 0;
+		const int displacement = 10;
+		string L() => new(' ', displacement + position + 4);
+		string R() => new(' ', displacement - position + 4);
+		string Ground =
+			new string(' ', 2) +
+			new string('-', displacement + (15 - displacement) + 2) +
+			new string('=', displacement * 2 + 2) +
+			new string('-', displacement + (15 - displacement) + 2) +
+			new string(' ', 2);
+		bool frame_a = false;
+		Console.Clear();
+		Console.WriteLine(@"
   Tug Of War
 
   Out pull your opponent in a rope pulling
-  competition. Mash the left and right arrow
-  keys to pull on the rope. First player to
-  pull the center into their boundary wins.
+  competition. Mash the [left]+[right] arrow
+  keys and/or the [A]+[D] keys to pull on the
+  rope. First player to pull the center of the
+  rope into their boundary wins.
 
   Choose Your Opponent:
-  [1] Easy.......1 mashes per second
-  [2] Medium.....2 mashes per second
-  [3] Hard.......4 mashes per second
-  [4] Harder.....8 mashes per second
+  [1] Easy.......2 mashes per second
+  [2] Medium.....4 mashes per second
+  [3] Hard.......8 mashes per second
+  [4] Harder....16 mashes per second
   [escape] give up");
-int? requiredMash = null;
-while (requiredMash is null)
-{
-	Console.CursorVisible = false;
-	switch (Console.ReadKey(true).Key)
-	{
-		case ConsoleKey.D1 or ConsoleKey.NumPad1: requiredMash = 1; break;
-		case ConsoleKey.D2 or ConsoleKey.NumPad2: requiredMash = 2; break;
-		case ConsoleKey.D3 or ConsoleKey.NumPad3: requiredMash = 4; break;
-		case ConsoleKey.D4 or ConsoleKey.NumPad4: requiredMash = 8; break;
-		case ConsoleKey.Escape: return;
-	}
-}
-Console.Clear();
-int mash = 0;
-int sleeps = 0;
-ConsoleKey lastKey = default;
-while (true)
-{
-	while (Console.KeyAvailable)
-	{
-		ConsoleKey key = Console.ReadKey(true).Key;
-		switch (key, lastKey)
+		int? requiredMash = null;
+		while (requiredMash is null)
 		{
-			case (ConsoleKey.Escape, _): return;
-			case (ConsoleKey.LeftArrow, ConsoleKey.RightArrow) or
-				 (ConsoleKey.RightArrow, ConsoleKey.LeftArrow):
-				mash++; lastKey = default; break;
-			default: lastKey = key; break;
+			Console.CursorVisible = false;
+			switch (Console.ReadKey(true).Key)
+			{
+				case ConsoleKey.D1 or ConsoleKey.NumPad1: requiredMash = 02; break;
+				case ConsoleKey.D2 or ConsoleKey.NumPad2: requiredMash = 04; break;
+				case ConsoleKey.D3 or ConsoleKey.NumPad3: requiredMash = 08; break;
+				case ConsoleKey.D4 or ConsoleKey.NumPad4: requiredMash = 16; break;
+				case ConsoleKey.Escape: return;
+			}
+		}
+		Console.Clear();
+		int mash = 0;
+		int presses = 0;
+		int sleeps = 0;
+		ConsoleKey lastKey = default;
+		DateTime start = DateTime.Now;
+		while (true)
+		{
+			while (Console.KeyAvailable)
+			{
+				ConsoleKey key = Console.ReadKey(true).Key;
+				if (key is ConsoleKey.Escape)
+				{
+					return;
+				}
+				else if (lastKey is not default(ConsoleKey) &&
+					key is ConsoleKey.A or ConsoleKey.D or ConsoleKey.LeftArrow or ConsoleKey.RightArrow &&
+					key != lastKey)
+				{
+					presses++;
+					mash++;
+					lastKey = default;
+				}
+				else if (key is ConsoleKey.A or ConsoleKey.D or ConsoleKey.LeftArrow or ConsoleKey.RightArrow)
+				{
+					lastKey = key;
+				}
+			}
+			if (sleeps is 2)
+			{
+				position = mash < requiredMash.Value
+					? position + 1
+					: position - 1;
+				sleeps = 0;
+				mash = 0;
+				if (Math.Abs(position) >= displacement)
+				{
+					break;
+				}
+			}
+			Console.CursorVisible = false;
+			Console.SetCursorPosition(0, 0);
+			Console.WriteLine();
+			Console.WriteLine("  Tug Of War");
+			Console.WriteLine();
+			Console.Write(frame_a
+				?
+				$@"{L()}o                             o {R()}{"\n"}" +
+				$@"{L()}LL-------------+-------------JJ\{R()}{"\n"}" +
+				$@"{L()}\\                            //{R()}{"\n"}" +
+				$@"{L()}| \                          / |{R()}{"\n"}"
+				:
+				$@"{L()} o                             o{R()}{"\n"}" +
+				$@"{L()}/LL-------------+-------------JJ{R()}{"\n"}" +
+				$@"{L()}\\                            //{R()}{"\n"}" +
+				$@"{L()}| \                          / |{R()}{"\n"}");
+			Console.WriteLine(Ground);
+			Console.WriteLine();
+			Console.WriteLine(frame_a
+				? "           *** Mash [A]+[D] or [Left]+[Right] ***"
+				: "           ''' Mash [A]+[D] or [Left]+[Right] '''");
+			Thread.Sleep(500);
+			sleeps++;
+			frame_a = !frame_a;
+		}
+		bool win = position < 0;
+		double seconds = (DateTime.Now - start).TotalSeconds;
+		double average = presses / seconds;
+		Console.Clear();
+		Console.WriteLine();
+		Console.WriteLine("  Tug Of War");
+		Console.WriteLine();
+		Console.Write(win
+			?
+			$@"{L()}o                               {R()}{"\n"}" +
+			$@"{L()}LL------------+------.  o___    {R()}{"\n"}" +
+			$@"{L()}\\                    \//   \\__{R()}{"\n"}" +
+			$@"{L()}| \                    \_____\  {R()}{"\n"}"
+			:
+			$@"{L()}                               o{R()}{"\n"}" +
+			$@"{L()}    ___o  .------+------------JJ{R()}{"\n"}" +
+			$@"{L()}__//   \\/                    //{R()}{"\n"}" +
+			$@"{L()}  /_____/                    / |{R()}{"\n"}");
+		Console.WriteLine(Ground);
+		Console.WriteLine();
+		Console.WriteLine("  You " + (win ? "Win!" : "Lose!"));
+		Console.WriteLine($"  Average: {average:0.##} mashes per second");
+		Console.WriteLine("  [enter] return to menu");
+		Console.WriteLine("  [escape] exit game");
+		bool enterPressed = false;
+		while (!enterPressed)
+		{
+			switch (Console.ReadKey(true).Key)
+			{
+				case ConsoleKey.Enter: enterPressed = true; break;
+				case ConsoleKey.Escape: return;
+			}
 		}
 	}
-	if (sleeps is 2)
-	{
-		position = mash < requiredMash.Value
-			? position + 1
-			: position - 1;
-		sleeps = 0;
-		mash = 0;
-	}
-	Console.CursorVisible = false;
-	Console.SetCursorPosition(0, 0);
-	Console.WriteLine();
-	Console.WriteLine("  Tug Of War");
-	Console.WriteLine();
-	string l = new(' ', displacement + position + 4);
-	string r = new(' ', displacement - position + 4);
-	Console.Write((frame_a = !frame_a)
-		?
-		$@"{l}o                             o {r}{"\n"}" +
-		$@"{l}LL-------------+-------------JJ\{r}{"\n"}" +
-		$@"{l}\\                            //{r}{"\n"}" +
-		$@"{l}| \                          / |{r}{"\n"}"
-		:
-		$@"{l} o                             o{r}{"\n"}" +
-		$@"{l}/LL-------------+-------------JJ{r}{"\n"}" +
-		$@"{l}\\                            //{r}{"\n"}" +
-		$@"{l}| \                          / |{r}{"\n"}");
-	Console.WriteLine(
-		new string(' ', 2) +
-		new string('-', displacement + (15 - displacement) + 2) +
-		new string('=', displacement * 2 + 2) +
-		new string('-', displacement + (15 - displacement) + 2) +
-		new string(' ', 2));
-	if (Math.Abs(position) >= displacement)
-	{
-		break;
-	}
-	Thread.Sleep(500);
-	sleeps++;
 }
-#warning TODO: win/loss frame
-Console.WriteLine();
-Console.WriteLine("  You " + (position < 0 ? "Win!" : "Lose!"));
-Console.WriteLine("  [enter] return to menu");
-Console.WriteLine("  [escape] exit game");
-GetEnterOrEscape:
-switch (Console.ReadKey(true).Key)
+finally
 {
-	case ConsoleKey.Enter: goto Restart;
-	case ConsoleKey.Escape: return;
-	default: goto GetEnterOrEscape;
+	Console.CursorVisible = true;
+	Console.Clear();
+	Console.Write("Tug Of War was closed.");
 }
