387b7c1bccacae10f3d8ebf54b1211ddba09c631
Zachary Patten <sevenix.zp@gmail.com>
2022-06-21 02:58:22 -0400

    adjustments to output

--- Projects/Checkers/Program.cs
+++ Projects/Checkers/Program.cs
@@ -27,36 +27,37 @@ finally
 void ShowIntroScreenAndGetOption()
 {
 	Console.Clear();
-	Console.WriteLine("CHECKERS");
 	Console.WriteLine();
-	Console.WriteLine("Checkers is  played on an 8x8  board between two sides commonly known as black");
-	Console.WriteLine("and white. The objective is  simple - capture  all  your opponent's pieces. An");
-	Console.WriteLine("alternative way to  win is to trap  your opponent so that  they have no  valid");
-	Console.WriteLine("moves left.");
+	Console.WriteLine("  Checkers");
 	Console.WriteLine();
-	Console.WriteLine("Black starts first and players take it  in turns to move their pieces  forward");
-	Console.WriteLine("across the board diagonally. Should a piece  reach the other side of the board");
-	Console.WriteLine("the piece becomes a `king` and can  then move diagonally backwards as  well as");
-	Console.WriteLine("forwards.");
+	Console.WriteLine("  Checkers is  played on an 8x8  board between two sides commonly known as black");
+	Console.WriteLine("  and white. The objective is  simple - capture  all  your opponent's pieces. An");
+	Console.WriteLine("  alternative way to  win is to trap  your opponent so that  they have no  valid");
+	Console.WriteLine("  moves left.");
 	Console.WriteLine();
-	Console.WriteLine("Pieces are captured by jumping over them diagonally. More than one enemy piece");
-	Console.WriteLine("can be captured in the same turn by the same piece.");
+	Console.WriteLine("  Black starts first and players take it  in turns to move their pieces  forward");
+	Console.WriteLine("  across the board diagonally. Should a piece  reach the other side of the board");
+	Console.WriteLine("  the piece becomes a `king` and can  then move diagonally backwards as  well as");
+	Console.WriteLine("  forwards.");
 	Console.WriteLine();
-	Console.WriteLine("Moves  are selected  with the arrow keys. Use the [enter] button to select the");
-	Console.WriteLine("from and to squares. Invalid moves are ignored.");
+	Console.WriteLine("  Pieces are captured by jumping over them diagonally. More than one enemy piece");
+	Console.WriteLine("  can be captured in the same turn by the same piece.");
 	Console.WriteLine();
-	Console.WriteLine("3 modes of play are possible depending on the number of players entered:");
+	Console.WriteLine("  Moves  are selected  with the arrow keys. Use the [enter] button to select the");
+	Console.WriteLine("  from and to squares. Invalid moves are ignored.");
+	Console.WriteLine();
+	Console.WriteLine("  3 modes of play are possible depending on the number of players entered:");
 	Console.WriteLine("    0 - black and white are controlled by the computer");
 	Console.WriteLine("    1 - black is controlled by the player and white by the computer");
 	Console.WriteLine("    2 - allows 2 players");
 	Console.WriteLine();
-	Console.Write("Enter the number of players (0-2): ");
+	Console.Write("  Enter the number of players (0-2): ");
 
 	string? entry = Console.ReadLine()?.Trim();
 	while (entry is not "0" and not "1" and not "2")
 	{
-		Console.WriteLine("Invalid Input. Try Again.");
-		Console.Write("Enter the number of players (0-2): ");
+		Console.WriteLine("  Invalid Input. Try Again.");
+		Console.Write("  Enter the number of players (0-2): ");
 		entry = Console.ReadLine()?.Trim();
 	}
 	int humanPlayerCount = entry[0] - '0';
@@ -115,7 +116,7 @@ void HandleGameOver()
 
 void PressAnyKeyToContinue()
 {
-	const string prompt = "Press any key to cotinue...";
+	const string prompt = "  Press any key to cotinue...";
 	(int left, int top) = (Console.CursorLeft, Console.CursorTop);
 	Console.Write(prompt);
 	Console.ReadKey(true);
@@ -134,6 +135,9 @@ void RenderGameState(Player? playerMoved = null, (int X, int Y)? selection = nul
 	}
 	char C(int x, int y) => (x, y) == selection ? '$' : tiles.GetValueOrDefault((x, y), '.');
 	StringBuilder sb = new();
+	sb.AppendLine();
+	sb.AppendLine("  Checkers");
+	sb.AppendLine();
 	sb.AppendLine($"    ╔═══════════════════╗");
 	sb.AppendLine($"  8 ║  {C(0, 7)} {C(1, 7)} {C(2, 7)} {C(3, 7)} {C(4, 7)} {C(5, 7)} {C(6, 7)} {C(7, 7)}  ║ {BlackPiece} = Black");
 	sb.AppendLine($"  7 ║  {C(0, 6)} {C(1, 6)} {C(2, 6)} {C(3, 6)} {C(4, 6)} {C(5, 6)} {C(6, 6)} {C(7, 6)}  ║ {BlackKing} = Black King");
@@ -145,21 +149,22 @@ void RenderGameState(Player? playerMoved = null, (int X, int Y)? selection = nul
 	sb.AppendLine($"  1 ║  {C(0, 0)} {C(1, 0)} {C(2, 0)} {C(3, 0)} {C(4, 0)} {C(5, 0)} {C(6, 0)} {C(7, 0)}  ║ {game.TakenCount(PieceColor.Black),2} x {BlackPiece}");
 	sb.AppendLine($"    ╚═══════════════════╝");
 	sb.AppendLine($"       A B C D E F G H");
+	sb.AppendLine();
 	if (selection is not null)
 	{
 		sb.Replace(" $ ", $"[{tiles.GetValueOrDefault(selection.Value, '.')}]");
 	}
 	if (game.Winner is not null)
 	{
-		sb.AppendLine($"*** {game.Winner} wins ***");
+		sb.AppendLine($"  *** {game.Winner} wins ***");
 	}
 	else if (playerMoved is not null)
 	{
-		sb.AppendLine($"{playerMoved.Color} moved");
+		sb.AppendLine($"  {playerMoved.Color} moved");
 	}
 	else
 	{
-		sb.AppendLine($"{game.Turn}'s turn");
+		sb.AppendLine($"  {game.Turn}'s turn");
 	}
 	Console.Write(sb);
 }
