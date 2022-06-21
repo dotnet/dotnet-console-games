1592ee1b5feb1c910d858348b41f2724f4f1638b
Zachary Patten <sevenix.zp@gmail.com>
2022-06-21 19:58:30 -0400

    global using static Black/White

--- Projects/Checkers/_using.cs
+++ Projects/Checkers/_using.cs
@@ -1,3 +1,12 @@
 ﻿global using System;
 global using System.Collections.Generic;
 global using System.Linq;
+global using System.Text;
+global using Checkers;
+global using static _using;
+
+public static class _using
+{
+	public const PieceColor Black = PieceColor.Black;
+	public const PieceColor White = PieceColor.White;
+}
