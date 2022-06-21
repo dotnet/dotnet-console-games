d235f3657fb3e2cabb618ca0d0fc18672fed62f3
Zachary Patten <sevenix.zp@gmail.com>
2022-06-20 22:17:40 -0400

    move Player initialization logic into Game.cs

--- Projects/Checkers/Game.cs
+++ Projects/Checkers/Game.cs
@@ -4,21 +4,23 @@ public class Game
 {
 	private const int PiecesPerSide = 12;
 
-	public PieceColor Turn { get; set; }
-	public Board Board { get; }
+	public PieceColor Turn { get; private set; }
+	public Board Board { get; private set; }
 	public int MoveCount { get; private set; }
-	public PieceColor Winner { get; set; } = PieceColor.NotSet;
+	public PieceColor Winner { get; private set; }
+	public List<Player> Players { get; private set; }
 
-	public List<Player> Players { get; set; } = new()
-		{
-			new Player { ControlledBy = PlayerControl.Computer, Color = PieceColor.Black },
-			new Player { ControlledBy = PlayerControl.Computer, Color = PieceColor.White }
-		};
-
-	public Game()
+	public Game(int humanPlayerCount)
 	{
 		Board = new Board();
+		Players = new()
+		{
+			new Player { ControlledBy = humanPlayerCount < 1 ? PlayerControl.Computer : PlayerControl.Human, Color = PieceColor.Black },
+			new Player { ControlledBy = humanPlayerCount < 2 ? PlayerControl.Computer : PlayerControl.Human, Color = PieceColor.White },
+		};
 		Turn = PieceColor.Black;
+		MoveCount = 0;
+		Winner = PieceColor.NotSet;
 	}
 
 	public MoveOutcome NextRound((int X, int Y)? from = null, (int X, int Y)? to = null)
