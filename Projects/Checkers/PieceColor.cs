aa1ddf68eae68966bf2af5fdd1579db931719d92
Zachary Patten <sevenix.zp@gmail.com>
2022-06-21 03:11:39 -0400

    moved enums out of Enums folder (if there are only two types in there I don't think a folder is necessary)

--- Projects/Checkers/PieceColor.cs
+++ Projects/Checkers/PieceColor.cs
@@ -1,4 +1,4 @@
-﻿namespace Checkers.Enums;
+﻿namespace Checkers;
 
 public enum PieceColor
 {
--- Projects/Checkers/_using.cs
+++ Projects/Checkers/_using.cs
@@ -1,4 +1,3 @@
 ﻿global using System;
 global using System.Collections.Generic;
 global using System.Linq;
-global using Checkers.Enums;
