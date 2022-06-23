0c52469e3502604179eefef53bdb5aa6f263918d
Zachary Patten <sevenix.zp@gmail.com>
2022-06-22 22:12:10 -0400

    Checkers blazor port

---
+++ Projects/Website/Games/Checkers/Player.cs
@@ -0,0 +1,13 @@
+﻿namespace Website.Games.Checkers;
+
+public class Player
+{
+	public bool IsHuman { get; private set; }
+	public PieceColor Color { get; private set; }
+
+	public Player(bool isHuman, PieceColor color)
+	{
+		IsHuman = isHuman;
+		Color = color;
+	}
+}
