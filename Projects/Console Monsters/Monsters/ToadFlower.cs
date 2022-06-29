2e28e18fcfa0990a1a8efe4f91bb1f08e54e8cd1
Zachary Patten <sevenix.zp@gmail.com>
2022-06-29 17:54:40 -0400

    monsters updates

---
+++ Projects/Console Monsters/Monsters/ToadFlower.cs
@@ -0,0 +1,16 @@
+﻿namespace Console_Monsters.Monsters;
+
+internal class ToadFlower : MonsterBase
+{
+	public ToadFlower()
+	{
+		Sprite = (
+			@"       . . . .   . . . .        " + '\n' +
+			@"        \~\~\~\~/~/~/~/         " + '\n' +
+			@"         \ \╭─────╮/ /          " + '\n' +
+			@"   ╔═╗ ╔══\ │ ‾o‾ │ /══╗ ╔═╗    " + '\n' +
+			@"     ╚═╝ ╭──╰─────╯──╮ ╚═╝      " + '\n' +
+			@"         │  ╭─────╮  │          " + '\n' +
+			@"         ╰──╯─╯ ╰─╰──╯          ").Split('\n');
+	}
+}
