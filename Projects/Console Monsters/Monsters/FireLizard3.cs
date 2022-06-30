00948a664fa82e2483c0fde14f811297253064ae
Zachary Patten <sevenix.zp@gmail.com>
2022-06-30 19:43:55 -0400

    sprites + monsters

---
+++ Projects/Console Monsters/Monsters/FireLizard3.cs
@@ -0,0 +1,19 @@
+namespace Console_Monsters.Monsters;
+
+internal class FireLizard3 : MonsterBase
+{
+	public FireLizard3()
+	{
+		Sprite = (
+			@"     ╭─╮─╮      " + '\n' +
+			@"    ╭╯ ╰╮╰╮     " + '\n' +
+			@"   ╭╯   ╰╮│╭───╮" + '\n' +
+			@"   ╰╮    │││‾o‾│" + '\n' +
+			@"╰╮  ╰─╮  │││   │" + '\n' +
+			@"╰╮╰╮  ╰╮ ╰┴╯│ ││" + '\n' +
+			@"╰╮╰╮╰╮ ╰──╮ ╰─╯│" + '\n' +
+			@"   ╰──────╯   ╭╯" + '\n' +
+			@"         ╰┬╮ ╭╯ " + '\n' +
+			@"          ╰╰─╯  ").Split('\n');
+	}
+}
