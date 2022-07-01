b9ff57c5a5c077d8edf6372abd7e9067f7bf1f3f
Zachary Patten <sevenix.zp@gmail.com>
2022-07-01 12:54:13 -0400

    monster updates

---
+++ Projects/Console Monsters/Monsters/RockBall1.cs
@@ -0,0 +1,14 @@
+﻿namespace Console_Monsters.Monsters;
+
+internal class RockBall1 : MonsterBase
+{
+	public RockBall1()
+	{
+		Name = "Rock Ball";
+	}
+
+	public override string[] Sprite => (
+		"  ╭─────╮  " + '\n' +
+		"╭─┤ ^_^ ├─╮" + '\n' +
+		"╰─╰─────╯─╯").Split('\n');
+}
