0df60542c32cd13ff22a6886d433f06c8bb3a5a0
Zachary Patten <sevenix.zp@gmail.com>
2022-07-01 14:58:26 -0400

    xml comments

--- Projects/Console Monsters/Maps/Map.cs
+++ Projects/Console Monsters/Maps/Map.cs
@@ -2,6 +2,7 @@
 
 public abstract class Map
 {
+	/// <summary>Converts from world (character) coordinates to tile coordinates.</summary>
 	public static (int I, int J) WorldToTile(int i, int j)
 	{
 		int tilei = i < 0 ? (i - 6) / 7 : i / 7;
@@ -9,6 +10,8 @@ public abstract class Map
 		return (tilei, tilej);
 	}
 
+	/// <summary>Relocates the player to the top-left most occurence of a character in a <see cref="Map.SpriteSheet"/>.</summary>
+	/// <param name="c"></param>
 	public static void SpawnCharacterOn(char c)
 	{
 		var (i, j) = FindTileInMap(map, c)!.Value;
@@ -16,6 +19,7 @@ public abstract class Map
 		character.J = j * 5;
 	}
 
+	/// <summary>Finds the top-left most occurence of a character in a <see cref="Map.SpriteSheet"/>.</summary>
 	public static (int I, int J)? FindTileInMap(Map map, char c)
 	{
 		char[][] s = map.SpriteSheet;
@@ -32,12 +36,16 @@ public abstract class Map
 		return null;
 	}
 
+	/// <summary>Gets the sprite for a given tile coordinate in the <see cref="Map"/>.</summary>
 	public abstract string GetMapTileRender(int tileI, int tileJ);
 
+	/// <summary>Determines if the given tile coordinates are valid for the player to move to.</summary>
 	public abstract bool IsValidCharacterMapTile(int tileI, int tileJ);
 
+	/// <summary>Interacts with adjacent tiles if there is anything to interact with.</summary>
 	public abstract void InteractWithMapTile(int tileI, int tileJ);
 
+	/// <summary>If necessary, performs an action when the player moves onto a tile.</summary>
 	public abstract void PerformTileAction();
 
 	public abstract char[][] SpriteSheet { get; }
