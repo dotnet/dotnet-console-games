b8784ba82d6c030cfc8a4f02243ff986140625dd
Ian Pike <Ianpike98@gmail.com>
2022-07-03 15:53:05 -0400

    Create King.cs

---
+++ Chess/Pieces/King.cs
@@ -0,0 +1,51 @@
+namespace Chess;
+
+public class King : Piece
+{
+	public override string Token { get { return "♔"; } }
+
+	public King(PieceColor color) : base(color) { }
+
+	public override void InitRules()
+	{
+		Rules.Add(new MovementRule(
+			m => m.EndX == m.StartX + 1,
+			m => m.EndY == m.StartY + 1
+		));
+
+		Rules.Add(new MovementRule(
+			m => m.EndX == m.StartX - 1,
+			m => m.EndY == m.StartY - 1
+		));
+
+		Rules.Add(new MovementRule(
+			m => m.EndX == m.StartX - 1,
+			m => m.EndY == m.StartY + 1
+		));
+
+		Rules.Add(new MovementRule(
+			m => m.EndX == m.StartX + 1,
+			m => m.EndY == m.StartY - 1
+		));
+
+		Rules.Add(new MovementRule(
+			m => m.EndX == m.StartX + 1,
+			m => m.EndY == m.StartY
+		));
+
+		Rules.Add(new MovementRule(
+			m => m.EndX == m.StartX,
+			m => m.EndY == m.StartY + 1
+		));
+
+		Rules.Add(new MovementRule(
+			m => m.EndX == m.StartX - 1,
+			m => m.EndY == m.StartY
+		));
+
+		Rules.Add(new MovementRule(
+			m => m.EndX == m.StartX,
+			m => m.EndY == m.StartY - 1
+		));
+	}
+}
