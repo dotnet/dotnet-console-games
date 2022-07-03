15dbd64918b0f5ebc94e11a4f8337416252a59e8
Ian Pike <Ianpike98@gmail.com>
2022-07-03 15:37:48 -0400

    Fixed some typos and spacing

--- Chess/Pieces/Knight.cs
+++ Chess/Pieces/Knight.cs
@@ -8,44 +8,44 @@ public class Knight : Piece
 
 	public override void InitRules()
 	{
-		Rules.Add(new Rule(
-                    m => m.EndX == m.StartX + 2,
-                    m => m.EndY == m.StartY + 1
-        ));
+		Rules.Add(new MovementRule(
+			m => m.EndX == m.StartX + 2,
+			m => m.EndY == m.StartY + 1
+		));
 
-        Rules.Add(new Rule(
-                    m => m.EndX == m.StartX + 2,
-                    m => m.EndY == m.StartY - 1
-        ));
+		Rules.Add(new MovementRule(
+			m => m.EndX == m.StartX + 2,
+			m => m.EndY == m.StartY - 1
+		));
 
-        Rules.Add(new Rule(
-                    m => m.EndX == m.StartX - 2,
-                    m => m.EndY == m.StartY + 1
-        ));
+		Rules.Add(new MovementRule(
+			m => m.EndX == m.StartX - 2,
+			m => m.EndY == m.StartY + 1
+		));
 
-        Rules.Add(new Rule(
-                    m => m.EndX == m.StartX - 2,
-                    m => m.EndY == m.StartY - 1
-        ));
-                        
-        Rules.Add(new Rule(
-                    m => m.EndX == m.StartX + 1,
-                    m => m.EndY == m.StartY + 2
-        ));
+		Rules.Add(new MovementRule(
+			m => m.EndX == m.StartX - 2,
+			m => m.EndY == m.StartY - 1
+		));
 
-        Rules.Add(new Rule(
-                    m => m.EndX == m.StartX - 1,
-                    m => m.EndY == m.StartY + 2
-        ));
+		Rules.Add(new MovementRule(
+			m => m.EndX == m.StartX + 1,
+			m => m.EndY == m.StartY + 2
+		));
 
-        Rules.Add(new Rule(
-                    m => m.EndX == m.StartX - 1,
-                    m => m.EndY == m.StartY - 2
-        ));
+		Rules.Add(new MovementRule(
+			m => m.EndX == m.StartX - 1,
+			m => m.EndY == m.StartY + 2
+		));
 
-        Rules.Add(new Rule(
-                    m => m.EndX == m.StartX + 1,
-                    m => m.EndY == m.StartY - 2
-        )); 
-    }
+		Rules.Add(new MovementRule(
+			m => m.EndX == m.StartX - 1,
+			m => m.EndY == m.StartY - 2
+		));
+
+		Rules.Add(new MovementRule(
+			m => m.EndX == m.StartX + 1,
+			m => m.EndY == m.StartY - 2
+		));
+	}
 }
