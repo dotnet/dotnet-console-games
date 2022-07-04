0e656051f5a59cbd565d33784754b20023545ce8
Zachary Patten <sevenix.zp@gmail.com>
2022-07-04 14:59:34 -0400

    Added Butterfly

---
+++ Projects/Console Monsters/Monsters/Butterfly.cs
@@ -0,0 +1,19 @@
+﻿namespace Console_Monsters.Monsters;
+
+public class Butterfly : MonsterBase
+{
+	public Butterfly()
+	{
+		Name = "Butterfly";
+	}
+
+	public override string[] Sprite => (
+		"       ╭┴─┴╮       " + '\n' +
+		" ╭╭─╮╮ │‾o‾│ ╭╭─╮╮ " + '\n' +
+		"╭╭╯|╰╮╮╰┬─┬╯╭╭╯|╰╮╮" + '\n' +
+		"╭╯♦|♦╰─╮├ ┤╭─╯♦|♦╰╮" + '\n' +
+		"╰╮♦|♦╭─╯├ ┤╰─╮♦|♦╭╯" + '\n' +
+		"╰╰╮|╭╯╯ ├ ┤ ╰╰╮|╭╯╯" + '\n' +
+		" ╰╰─╯╯  ├ ┤  ╰╰─╯╯ " + '\n' +
+		"        ╰╥╯        ").Split('\n');
+}
