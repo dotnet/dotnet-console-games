64f61deb8c117d851a6253a55991b7a11f740400
Zachary Patten <sevenix.zp@gmail.com>
2022-07-04 14:18:48 -0400

    added Chopster monster

---
+++ Projects/Console Monsters/Monsters/Chopster.cs
@@ -0,0 +1,20 @@
+﻿namespace Console_Monsters.Monsters;
+
+internal class Chopster : MonsterBase
+{
+	public Chopster()
+	{
+		Name = "Chopster";
+	}
+
+public override string[] Sprite => (
+	"  ╔═════╗  " + '\n' +
+	"╷╷║     ║╷╷" + '\n' +
+	"└┴╢  ╳  ╟┴┘" + '\n' +
+	" ═╩╤═══╤╩═ " + '\n' +
+	"   │°ᴗ°│   " + '\n' +
+	"  ╭╰───╯╮  " + '\n' +
+	"  │├───┤│  " + '\n' +
+	"  ╹╰┬─┬╯╹  " + '\n' +
+	"   ━┘ └━   ").Split('\n');
+}
