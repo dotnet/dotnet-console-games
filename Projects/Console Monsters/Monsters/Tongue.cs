e03cd3ed2002db45fecd3218e0dc3d5aab966eae
Zachary Patten <sevenix.zp@gmail.com>
2022-07-05 20:35:31 -0400

    monsters

---
+++ Projects/Console Monsters/Monsters/Tongue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Monsters.Monsters;
+internal class Tongue : MonsterBase
+{
+	public Turtle()
+	{
+		Name = "Turtle";
+	}
+
+	public override string[] Sprite => (
+		"  ╭─────╮    " + '\n' +
+		"  │ ⱺ_ⱺ │    " + '\n' +
+		"╭─╰─┤ ├─╯─╮  " + '\n' +
+		"│ ╷╭┤ ├╮╷ │  " + '\n' +
+		"╰─╯││ ││╰─╯╭╮" + '\n' +
+		" ╭╯╰┤ ├╯╰╮─╯│" + '\n' +
+		" │ ╭┤ ├╮ ├──╯" + '\n' +
+		" ╰─╯│ │╰─╯   " + '\n' +
+		"    │ │      " + '\n' +
+		"    ╯─╰      ").Split('\n');
+}
