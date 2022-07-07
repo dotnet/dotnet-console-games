77663a537d8ad1f075578c31b66f2926e4997f68
HUNTERSCOUTI <39131351+HUNTERSCOUTI@users.noreply.github.com>
2022-07-08 01:09:04 +0200

    BattleSystem Update 2 NOT WORKING

--- Projects/Console Monsters/BattleSystem.cs
+++ Projects/Console Monsters/BattleSystem.cs
@@ -2,12 +2,18 @@
 
 public class BattleSystem
 {
-	// TEMP FOR DEVELOPMENT
-	static FireLizard fireLizard = new();
-	static Turtle turtle = new();
+	// TEMP FOR DEVELOPMENT, YES VERY MESSY, PLEASE FIX
+#pragma warning disable CS8618
+	public static FireLizard fireLizard = new();
+	public static Turtle turtle = new();
 
 	public static MonsterBase PlayerMonster = turtle;
 	public static MonsterBase OpponentMonster = fireLizard;
+
+	public static MonsterBase AttackingMonster;
+	public static MonsterBase DefendingMonster;
+#pragma warning restore CS8618
+
 	public static void Battle()
 	{
 		bool playerTurn = true;
@@ -17,40 +23,73 @@ public class BattleSystem
 		{
 			playerTurn = false;
 		}
+		DrawStats(playerTurn);
 		while (!battleOver)
 		{
-			while (playerTurn)
+			if (playerTurn)
 			{
-				MoveBase playerMove = MoveBase.GetRandomMove();
-				PlayerMonster.CurrentEnergy -= playerMove.EnergyTaken;
-				OpponentMonster.CurrentHP -= (int)playerMove.FinalDamage;
+				AttackingMonster = PlayerMonster;
+				DefendingMonster = OpponentMonster;
 				if (PlayerMonster.CurrentHP <= 0)
 				{
 					Console.Clear();
-					Console.Write("Player Lost");
+					Console.WriteLine("Player Lost");
+					battleOver = true;
 				}
 				else
 				{
+
+					PressEnterToContiue();
+					MoveBase playerMove = MoveBase.GetRandomMove();
+					PlayerMonster.CurrentEnergy -= playerMove.EnergyTaken;
+					OpponentMonster.CurrentHP -= (int)playerMove.FinalDamage;
 					playerTurn = false;
+					DrawStats(playerTurn);
 				}
-				PressEnterToContiue();
 			}
-			while (!playerTurn)
+			if (!playerTurn)
 			{
-				MoveBase opponentMove = MoveBase.GetRandomMove();
-				OpponentMonster.CurrentEnergy -= opponentMove.EnergyTaken;
-				PlayerMonster.CurrentHP -= (int)opponentMove.FinalDamage;
-				if (PlayerMonster.CurrentHP <= 0)
+				AttackingMonster = OpponentMonster;
+				DefendingMonster = PlayerMonster;
+				if (OpponentMonster.CurrentHP <= 0)
 				{
 					Console.Clear();
-					Console.Write("Player Lost");
+					Console.WriteLine("CPU Lost");
+					battleOver = true;
 				}
 				else
 				{
+					PressEnterToContiue();
+					MoveBase opponentMove = MoveBase.GetRandomMove();
+					OpponentMonster.CurrentEnergy -= opponentMove.EnergyTaken;
+					PlayerMonster.CurrentHP -= (int)opponentMove.FinalDamage;
 					playerTurn = true;
+					DrawStats(playerTurn);
 				}
-				PressEnterToContiue();
 			}
 		}
+
+		void DrawStats(bool playerTurn)
+		{
+			//TEMP
+			Console.SetCursorPosition(63, 34);
+			Console.WriteLine($"HP:{PlayerMonster.CurrentHP}  Energy:{PlayerMonster.CurrentEnergy}  ");
+
+			Console.SetCursorPosition(102, 13);
+			Console.WriteLine($"HP:{OpponentMonster.CurrentHP}  Energy:{OpponentMonster.CurrentEnergy}  ");
+
+			Console.SetCursorPosition(35, 5);
+			string turn;
+			if (playerTurn)
+			{
+				turn = "Player Turn";
+			}
+			else
+			{
+				turn = "CPU Turn   ";
+			}
+			Console.WriteLine(turn);
+		}
+
 	}
 }
