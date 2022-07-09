8aa96fc6a9108b52d9b9f6b98cefd8e0dda06d21
Mohammed Bilal Al-Shalan <moha151h@edu.easj.dk>
2022-07-09 20:50:39 +0200

    #48: feat: Many new sprites. ranging from objects to npcs to monsters and etc. MonsterBox open and close once enter is pressed. First House and it's second floor has been added.

--- Projects/Console Monsters/Menus/KeyMapping.cs
+++ Projects/Console Monsters/Menus/KeyMapping.cs
@@ -6,6 +6,20 @@ public class KeyMapping
 	{
 		StringBuilder sb = new();
 
+		string[] MonsterBox =
+		{
+			//Closed
+			"╭───╮", //0
+			"╞═●═╡", //1
+			"╰───╯", //2
+			//Open
+			"╭─●─╮", //3
+			"├---┤", //4
+			"╰───╯", //5
+		};
+
+		int index = 0;
+
 		int arrowOption = 1;
 		const int maxOption = 6;
 
@@ -42,25 +56,25 @@ public class KeyMapping
 		sb.AppendLine(@$"{headerIndent}█▀▄▀█ █▄▄█ █ █▀▄ █   █   █▄▄█ █     █  ");
 		sb.AppendLine(@$"{headerIndent}█   █ █  █ █ █  ▀█   █   █  █ █▄▄   █  ");
 		sb.AppendLine(@$"{newLineOptions}");
-		sb.AppendLine(@$"{optionIndent}▄  ▄ ▄▄▄   {new(' ', 30)}{currentUp[0]}{new(' ', 10)}█{new(' ', 11)}{currentUpAlt[0]}   {(arrowOption is 1 ? "╭───╮" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█  █ █▄▄▀ ▀{new(' ', 30)}{currentUp[1]}{new(' ', 10)}█{new(' ', 11)}{currentUpAlt[1]}   {(arrowOption is 1 ? "╞═●═╡" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}▀▄▄▀ █    ▄{new(' ', 30)}{currentUp[2]}{new(' ', 10)}█{new(' ', 11)}{currentUpAlt[2]}   {(arrowOption is 1 ? "╰───╯" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}▄  ▄ ▄▄▄   {new(' ', 30)}{currentUp[0]}{new(' ', 10)}█{new(' ', 11)}{currentUpAlt[0]}   {(arrowOption is 1 ? MonsterBox[index]   : "     ")}");
+		sb.AppendLine(@$"{optionIndent}█  █ █▄▄▀ ▀{new(' ', 30)}{currentUp[1]}{new(' ', 10)}█{new(' ', 11)}{currentUpAlt[1]}   {(arrowOption is 1 ? MonsterBox[index+1] : "     ")}");
+		sb.AppendLine(@$"{optionIndent}▀▄▄▀ █    ▄{new(' ', 30)}{currentUp[2]}{new(' ', 10)}█{new(' ', 11)}{currentUpAlt[2]}   {(arrowOption is 1 ? MonsterBox[index+2] : "     ")}");
 		sb.AppendLine(@$"{newLineOptions}");
-		sb.AppendLine(@$"{optionIndent}▄▄▄   ▄▄  ▄   ▄ ▄   ▄  {new(' ', 18)}{currentDown[0]}{new(' ', 12 + currentDown[0].Length)}█{new(' ', 16)}{currentDownAlt[0]}   {(arrowOption is 2 ? "╭───╮" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█  █ █  █ █ ▄ █ █▀▄ █ ▀{new(' ', 18)}{currentDown[1]}{new(' ', 12 + currentDown[1].Length)}█{new(' ', 16)}{currentDownAlt[1]}   {(arrowOption is 2 ? "╞═●═╡" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█▄▄▀ ▀▄▄▀ █▀ ▀█ █  ▀█ ▄{new(' ', 18)}{currentDown[2]}{new(' ', 12 + currentDown[2].Length)}█{new(' ', 16)}{currentDownAlt[2]}   {(arrowOption is 2 ? "╰───╯" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}▄▄▄   ▄▄  ▄   ▄ ▄   ▄  {new(' ', 18)}{currentDown[0]}{new(' ', 12 + currentDown[0].Length)}█{new(' ', 16)}{currentDownAlt[0]}   {(arrowOption is 2 ? MonsterBox[index]   : "     ")}");
+		sb.AppendLine(@$"{optionIndent}█  █ █  █ █ ▄ █ █▀▄ █ ▀{new(' ', 18)}{currentDown[1]}{new(' ', 12 + currentDown[1].Length)}█{new(' ', 16)}{currentDownAlt[1]}   {(arrowOption is 2 ? MonsterBox[index+1] : "     ")}");
+		sb.AppendLine(@$"{optionIndent}█▄▄▀ ▀▄▄▀ █▀ ▀█ █  ▀█ ▄{new(' ', 18)}{currentDown[2]}{new(' ', 12 + currentDown[2].Length)}█{new(' ', 16)}{currentDownAlt[2]}   {(arrowOption is 2 ? MonsterBox[index+2] : "     ")}");
 		sb.AppendLine(@$"{newLineOptions}");
-		sb.AppendLine(@$"{optionIndent}▄   ▄▄▄▄ ▄▄▄▄ ▄▄▄▄▄            {currentLeft[0]}{new(' ', 12 + currentLeft[0].Length)}█{new(' ', 11)}{currentLeftAlt[0]}   {(arrowOption is 3 ? "╭───╮" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█   █▄▄  █▄▄    █   ▀          {currentLeft[1]}{new(' ', 12 + currentLeft[1].Length)}█{new(' ', 11)}{currentLeftAlt[1]}   {(arrowOption is 3 ? "╞═●═╡" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█▄▄ █▄▄▄ █      █   ▄          {currentLeft[2]}{new(' ', 12 + currentLeft[2].Length)}█{new(' ', 11)}{currentLeftAlt[2]}   {(arrowOption is 3 ? "╰───╯" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}▄   ▄▄▄▄ ▄▄▄▄ ▄▄▄▄▄            {currentLeft[0]}{new(' ', 12 + currentLeft[0].Length)}█{new(' ', 11)}{currentLeftAlt[0]}   {(arrowOption is 3 ? MonsterBox[index]   : "     ")}");
+		sb.AppendLine(@$"{optionIndent}█   █▄▄  █▄▄    █   ▀          {currentLeft[1]}{new(' ', 12 + currentLeft[1].Length)}█{new(' ', 11)}{currentLeftAlt[1]}   {(arrowOption is 3 ? MonsterBox[index+1] : "     ")}");
+		sb.AppendLine(@$"{optionIndent}█▄▄ █▄▄▄ █      █   ▄          {currentLeft[2]}{new(' ', 12 + currentLeft[2].Length)}█{new(' ', 11)}{currentLeftAlt[2]}   {(arrowOption is 3 ? MonsterBox[index+2] : "     ")}");
 		sb.AppendLine(@$"{newLineOptions}");
-		sb.AppendLine(@$"{optionIndent}▄▄▄  ▄  ▄▄▄  ▄  ▄ ▄▄▄▄▄        {currentRight[0]}{new(' ', 12 + currentRight[0].Length)}█{new(' ', 11)}{currentRightAlt[0]}   {(arrowOption is 4 ? "╭───╮" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█▄▄▀ █ █  ▄▄ █▄▄█   █   ▀      {currentRight[1]}{new(' ', 12 + currentRight[1].Length)}█{new(' ', 11)}{currentRightAlt[1]}   {(arrowOption is 4 ? "╞═●═╡" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█  █ █ ▀▄▄▄▀ █  █   █   ▄      {currentRight[2]}{new(' ', 12 + currentRight[2].Length)}█{new(' ', 11)}{currentRightAlt[2]}   {(arrowOption is 4 ? "╰───╯" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}▄▄▄  ▄  ▄▄▄  ▄  ▄ ▄▄▄▄▄        {currentRight[0]}{new(' ', 12 + currentRight[0].Length)}█{new(' ', 11)}{currentRightAlt[0]}   {(arrowOption is 4 ? MonsterBox[index]   : "     ")}");
+		sb.AppendLine(@$"{optionIndent}█▄▄▀ █ █  ▄▄ █▄▄█   █   ▀      {currentRight[1]}{new(' ', 12 + currentRight[1].Length)}█{new(' ', 11)}{currentRightAlt[1]}   {(arrowOption is 4 ? MonsterBox[index+1] : "     ")}");
+		sb.AppendLine(@$"{optionIndent}█  █ █ ▀▄▄▄▀ █  █   █   ▄      {currentRight[2]}{new(' ', 12 + currentRight[2].Length)}█{new(' ', 11)}{currentRightAlt[2]}   {(arrowOption is 4 ? MonsterBox[index+2] : "     ")}");
 		sb.AppendLine(@$"{newLineOptions}");
-		sb.AppendLine(@$"{optionIndent} ▄▄   ▄▄▄ ▄▄▄▄▄ ▄  ▄▄  ▄   ▄   {currentRight[0]}{new(' ', 12 + currentRight[0].Length)}█{new(' ', 11)}{currentRightAlt[0]}   {(arrowOption is 5 ? "╭───╮" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█▄▄█ █      █   █ █  █ █▀▄ █ ▀ {currentRight[1]}{new(' ', 12 + currentRight[1].Length)}█{new(' ', 11)}{currentRightAlt[1]}   {(arrowOption is 5 ? "╞═●═╡" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█  █ ▀▄▄▄   █   █ ▀▄▄▀ █  ▀█ ▄ {currentRight[2]}{new(' ', 12 + currentRight[2].Length)}█{new(' ', 11)}{currentRightAlt[2]}   {(arrowOption is 5 ? "╰───╯" : "     ")}");
+		sb.AppendLine(@$"{optionIndent} ▄▄   ▄▄▄ ▄▄▄▄▄ ▄  ▄▄  ▄   ▄   {currentRight[0]}{new(' ', 12 + currentRight[0].Length)}█{new(' ', 11)}{currentRightAlt[0]}   {(arrowOption is 5 ? MonsterBox[index]   : "     ")}");
+		sb.AppendLine(@$"{optionIndent}█▄▄█ █      █   █ █  █ █▀▄ █ ▀ {currentRight[1]}{new(' ', 12 + currentRight[1].Length)}█{new(' ', 11)}{currentRightAlt[1]}   {(arrowOption is 5 ? MonsterBox[index+1] : "     ")}");
+		sb.AppendLine(@$"{optionIndent}█  █ ▀▄▄▄   █   █ ▀▄▄▀ █  ▀█ ▄ {currentRight[2]}{new(' ', 12 + currentRight[2].Length)}█{new(' ', 11)}{currentRightAlt[2]}   {(arrowOption is 5 ? MonsterBox[index+2] : "     ")}");
 
 		Console.SetCursorPosition(0, 0);
 		Console.WriteLine(sb);
@@ -70,6 +84,9 @@ public class KeyMapping
 			case ConsoleKey.UpArrow or ConsoleKey.W: arrowOption = Math.Max(1, arrowOption - 1); goto ReDraw;
 			case ConsoleKey.DownArrow or ConsoleKey.S: arrowOption = Math.Min(maxOption, arrowOption + 1); goto ReDraw;
 			case ConsoleKey.Enter or ConsoleKey.E:
+
+			int MonsterBoxOpen = index == 0 ? index = 3 : index = 0;	
+		    
 				switch (arrowOption)
 				{
 					case 1: goto ReDraw;
