624998f8f30f671cd5fbad0b90b25daefbaddf00
Zachary Patten <sevenix.zp@gmail.com>
2022-07-12 19:23:16 -0400

    monsters

---
+++ Projects/Console Monsters/Monsters/Elephant.cs
@@ -0,0 +1,16 @@
+﻿namespace Console_Monsters.Monsters;
+
+public class Elephant : MonsterBase
+{
+	public Elephant()
+	{
+		Name = "Elephant";
+	}
+
+	public override string[] Sprite => (
+		@"  ╭───╮      " + '\n' +
+		@" C│^ ^│Ↄ───╮ " + '\n' +
+		@"╒╕╰┤├┬╯    ├╮" + '\n' +
+		@"╰╰─╯╯╰╮╮─╮╮╯ " + '\n' +
+		@"      └└ ┘┘  ").Split('\n');
+}
