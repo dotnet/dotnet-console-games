bab3e65ee18b1c6c9967f9e7f2ec69dda9c9e293
Zachary Patten <sevenix.zp@gmail.com>
2022-07-11 21:05:21 -0400

    new monsters and npc

---
+++ Projects/Console Monsters/Monsters/Squid.cs
@@ -0,0 +1,17 @@
+﻿namespace Console_Monsters.Monsters;
+
+internal class Squid : MonsterBase
+{
+	public Squid()
+	{
+		Name = "Squid";
+	}
+
+	public override string[] Sprite => (
+		@" ╭───╮  " + "\n" +
+		@" │^_^│  " + "\n" +
+		@"╭╰───╯─╮" + "\n" +
+		@"│╭╮╭╮╭╮│" + "\n" +
+		@"││││││││" + "\n" +
+		@"╰╰╰╯╰╯╯╯").Split('\n');
+}
