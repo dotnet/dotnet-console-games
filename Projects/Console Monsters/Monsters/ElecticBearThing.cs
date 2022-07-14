1c6514bb531e2f3d5a55c332dfe93d73a234abaf
Zachary Patten <sevenix.zp@gmail.com>
2022-07-14 01:17:37 -0400

    electric bear thing monster

---
+++ Projects/Console Monsters/Monsters/ElecticBearThing.cs
@@ -0,0 +1,20 @@
+﻿namespace Console_Monsters.Monsters;
+
+internal class ElecticBearThing : MonsterBase
+{
+	public ElecticBearThing()
+	{
+		Name = "Electic Bear Thing";
+	}
+
+	public override string[] Sprite => (
+		@"     ₒ   ₒ       " + '\n' +
+		@"    __╲_╱__      " + '\n' +
+		@"   ╱▬▬ⱺ♦ⱺ▬▬╲     " + '\n' +
+		@"╭─wwww/┅\wwww─╮  " + '\n' +
+		@"├~~╮  ╷▄╷  ╭~~┤  " + '\n' +
+		@"┝▬▬┽ww┤█├ww┾▬▬┥  " + '\n' +
+		@"╰╰╰│▀▀ █ ▀▀│╯╯╯ ₒ" + '\n' +
+		@"   ╰┬─┬▀┬─┬╯━─━─╯" + '\n' +
+		@"    ╰┅╯ ╰┅╯      ").Split('\n');
+}
