7536d9b3a0c9eb72f42ad2f9bd813cf077c473f5
HUNTERSCOUTI <39131351+HUNTERSCOUTI@users.noreply.github.com>
2022-07-14 22:43:40 +0200

    Menus folder

---
+++ Projects/Console Monsters/Screens/Menus/OptionsScreen.cs
@@ -0,0 +1,71 @@
+﻿namespace Console_Monsters.Screens;
+
+public static class OptionsScreen
+{
+	public static void OptionsMenu()
+	{
+		StringBuilder sb = new();
+
+		int arrowOption = 1;
+		const int maxOption = 5;
+
+		string optionIndent = new(' ', 50);
+		string titleIndent = new(' ', 50);
+		string newLineOptions = new('\n', 2);
+		string newLineTitle = new('\n', 6);
+
+		Console.Clear();
+	ReDraw:
+		sb.Clear();
+
+		sb.AppendLine(@$"{newLineTitle}");
+		sb.AppendLine(@$"{titleIndent} ██████╗ ██████╗ ████████╗██╗ ██████╗ ███╗   ██╗███████╗");
+		sb.AppendLine(@$"{titleIndent}██╔═══██╗██╔══██╗╚══██╔══╝██║██╔═══██╗████╗  ██║██╔════╝");
+		sb.AppendLine(@$"{titleIndent}██║   ██║██████╔╝   ██║   ██║██║   ██║██╔██╗ ██║███████╗");
+		sb.AppendLine(@$"{titleIndent}██║   ██║██╔═══╝    ██║   ██║██║   ██║██║╚██╗██║╚════██║");
+		sb.AppendLine(@$"{titleIndent}╚██████╔╝██║        ██║   ██║╚██████╔╝██║ ╚████║███████║");
+		sb.AppendLine(@$"{titleIndent} ╚═════╝ ╚═╝        ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝");
+		sb.AppendLine(@$"{newLineTitle}");
+
+		sb.AppendLine(@$"{optionIndent}{(DisableMovementAnimation ? "╔══╗" : "╔══╗")}                      {(arrowOption is 1 ? "╭───╮" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{(DisableMovementAnimation ? "║  ║" : "║██║")}  Movement Animation  {(arrowOption is 1 ? "╞═●═╡" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{(DisableMovementAnimation ? "╚══╝" : "╚══╝")}                      {(arrowOption is 1 ? "╰───╯" : "     ")}");
+		sb.AppendLine(@$"{newLineOptions}");
+		sb.AppendLine(@$"{optionIndent}{(DisableBattleTransition ? "╔══╗" : "╔══╗")}                     {(arrowOption is 2 ? "╭───╮" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{(DisableBattleTransition ? "║  ║" : "║██║")}  Battle Transition  {(arrowOption is 2 ? "╞═●═╡" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{(DisableBattleTransition ? "╚══╝" : "╚══╝")}                     {(arrowOption is 2 ? "╰───╯" : "     ")}");
+		sb.AppendLine(@$"{newLineOptions}");
+		sb.AppendLine(@$"{optionIndent}{(DisableBattle ? "╔══╗" : "╔══╗")}                      {(arrowOption is 3 ? "╭───╮" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{(DisableBattle ? "║  ║" : "║██║")}  Battles (DEV TOOL)  {(arrowOption is 3 ? "╞═●═╡" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{(DisableBattle ? "╚══╝" : "╚══╝")}                      {(arrowOption is 3 ? "╰───╯" : "     ")}");
+		sb.AppendLine(@$"{newLineOptions}");
+		sb.AppendLine(@$"{optionIndent}▄  ▄  ▄▄▄▄ ▄   ▄   ▄   ▄  ▄▄  ▄▄▄  ▄▄▄  ▄ ▄   ▄  ▄▄▄    {(arrowOption is 4 ? "╭───╮" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}█■█   █▄▄   ▀▄▀    █▀▄▀█ █▄▄█ █▄▄▀ █▄▄▀ █ █▀▄ █ █  ▄▄   {(arrowOption is 4 ? "╞═●═╡" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}█  ▀▄ █▄▄▄   █     █   █ █  █ █    █    █ █  ▀█ ▀▄▄▄▀   {(arrowOption is 4 ? "╰───╯" : "     ")}");
+		sb.AppendLine(@$"{newLineOptions}");
+		sb.AppendLine(@$"{optionIndent}█▀▀▄  ▄▄   ▄▄▄ ▄  ▄   {(arrowOption is maxOption ? "╭───╮" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}█■■█ █▄▄█ █    █■█    {(arrowOption is maxOption ? "╞═●═╡" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}█▄▄▀ █  █ ▀▄▄▄ █  ▀▄  {(arrowOption is maxOption ? "╰───╯" : "     ")}");
+
+		Console.SetCursorPosition(0, 0);
+		Console.WriteLine(sb);
+
+		switch (Console.ReadKey(true).Key)
+		{
+			case ConsoleKey.UpArrow or ConsoleKey.W: arrowOption = Math.Max(1, arrowOption - 1); goto ReDraw;
+			case ConsoleKey.DownArrow or ConsoleKey.S: arrowOption = Math.Min(maxOption, arrowOption + 1); goto ReDraw;
+			case ConsoleKey.Enter or ConsoleKey.E:
+				switch (arrowOption)
+				{
+					case 1: DisableMovementAnimation = !DisableMovementAnimation; goto ReDraw;
+					case 2: DisableBattleTransition = !DisableBattleTransition; goto ReDraw;
+					case 3: DisableBattle = !DisableBattle; goto ReDraw;
+					case 4: KeyMappingScreen.KeyMappingMenu(); break;
+					case maxOption: break;
+				}
+				break;
+			case ConsoleKey.Escape: break;
+			default: goto ReDraw;
+		}
+	}
+}
