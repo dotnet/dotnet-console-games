d78873cddb4ba9a7d5f9b033c66f915a7d9c7f3a
Ian Pike <Ianpike98@gmail.com>
2022-07-13 20:20:13 -0400

    Add color options
    Everything works in its current state, but the formatting needs a lot of work. Also, some of the more advanced functionality like being able to select between if you want to affect the foreground or the background has yet to be implemented, but the process should not be too challenging.

--- Projects/Console Monsters/Screens/ConsoleColorSettingsScreen.cs
+++ Projects/Console Monsters/Screens/ConsoleColorSettingsScreen.cs
@@ -1,8 +1,10 @@
+using Console_Monsters.Util;
+
 namespace Console_Monsters.Screens;
 
 public static class ConsoleColorSettingsScreen
 {
-	public static void KeyMappingMenu()
+	public static void ColorSchemeMenu()
 	{
 		StringBuilder sb = new();
 
@@ -13,58 +15,64 @@ public static class ConsoleColorSettingsScreen
 		string headerIndent = new(' ', 85);
 		string titleIndent = new(' ', 40);
 
-
+		var headerAsciiText = TextGenerator.GenerateAsciiText(@$"background {new(' ', 8)}|{new(' ', 8)} text");
+		var blackAsciiText = TextGenerator.GenerateAsciiText("black");
+		var whiteAsciiText = TextGenerator.GenerateAsciiText("white");
+		var greenAsciiText = TextGenerator.GenerateAsciiText("green");
+		var redAsciiText = TextGenerator.GenerateAsciiText("red");
+		var blueAsciiText = TextGenerator.GenerateAsciiText("blue");
+		var returnToOptionsText = TextGenerator.GenerateAsciiText("Back");
 		
-
 		Console.Clear();
 	ReDraw:
 		sb.Clear();
 
+		// TODO: Fix this formatting later when the ability to select individual colors is available
 		sb.Append('\n', 3); // SET TO 6 WHEN SCROLL
-		sb.AppendLine(@$"{titleIndent}██╗  ██╗███████╗██╗   ██╗    ███╗   ███╗ █████╗ ██████╗ ██████╗ ██╗███╗   ██╗ ██████╗ ");
-		sb.AppendLine(@$"{titleIndent}██║ ██╔╝██╔════╝╚██╗ ██╔╝    ████╗ ████║██╔══██╗██╔══██╗██╔══██╗██║████╗  ██║██╔════╝ ");
-		sb.AppendLine(@$"{titleIndent}█████╔╝ █████╗   ╚████╔╝     ██╔████╔██║███████║██████╔╝██████╔╝██║██╔██╗ ██║██║  ███╗");
-		sb.AppendLine(@$"{titleIndent}██╔═██╗ ██╔══╝    ╚██╔╝      ██║╚██╔╝██║██╔══██║██╔═══╝ ██╔═══╝ ██║██║╚██╗██║██║   ██║");
-		sb.AppendLine(@$"{titleIndent}██║  ██╗███████╗   ██║       ██║ ╚═╝ ██║██║  ██║██║     ██║     ██║██║ ╚████║╚██████╔╝");
-		sb.AppendLine(@$"{titleIndent}╚═╝  ╚═╝╚══════╝   ╚═╝       ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝     ╚═╝╚═╝  ╚═══╝ ╚═════╝ ");
+		sb.AppendLine(@$"{titleIndent}██████╗ ██████╗ ██╗      ██████╗ ██████╗     ███████╗ ██████╗██╗  ██╗███████╗███╗   ███╗███████╗");
+		sb.AppendLine(@$"{titleIndent}██╔════╝██╔═══██╗██║     ██╔═══██╗██╔══██╗    ██╔════╝██╔════╝██║  ██║██╔════╝████╗ ████║██╔════╝");
+		sb.AppendLine(@$"{titleIndent}██║     ██║   ██║██║     ██║   ██║██████╔╝    ███████╗██║     ███████║█████╗  ██╔████╔██║█████╗  ");
+		sb.AppendLine(@$"{titleIndent}██║     ██║   ██║██║     ██║   ██║██╔══██╗    ╚════██║██║     ██╔══██║██╔══╝  ██║╚██╔╝██║██╔══╝  ");
+		sb.AppendLine(@$"{titleIndent}╚██████╗╚██████╔╝███████╗╚██████╔╝██║  ██║    ███████║╚██████╗██║  ██║███████╗██║ ╚═╝ ██║███████╗");
+		sb.AppendLine(@$"{titleIndent} ╚═════╝ ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═╝    ╚══════╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝╚══════╝");
 		sb.Append('\n', 3); // SET TO 6 WHEN SCROLL
-		sb.AppendLine($@"{headerIndent}▄   ▄  ▄▄  ▄ ▄   ▄   █    ▄▄  ▄   ▄▄▄▄▄");
-		sb.AppendLine(@$"{headerIndent}█▀▄▀█ █▄▄█ █ █▀▄ █   █   █▄▄█ █     █  ");
-		sb.AppendLine(@$"{headerIndent}█   █ █  █ █ █  ▀█   █   █  █ █▄▄   █  ");
+		sb.AppendLine($@"{headerIndent}{headerAsciiText[0]}");
+		sb.AppendLine(@$"{headerIndent}{headerAsciiText[1]}");
+		sb.AppendLine(@$"{headerIndent}{headerAsciiText[2]}");
 		sb.AppendLine();
-		sb.AppendLine($@"{new(' ', 89)}{(upOption == 1 ? boxTop : boxEmpty)}{new(' ', 18)}{boxTop}");
-		sb.AppendLine(@$"{optionIndent}▄  ▄ ▄▄▄   {new(' ', 28)}{boxSide} {currentUp[0]} {boxSide}{new(' ', 8)}█{new(' ', 11)}{currentUpAlt[0]}   {(arrowOption is 1 ? "╭───╮" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█  █ █▄▄▀ ▀{new(' ', 28)}{boxSide} {currentUp[1]} {boxSide}{new(' ', 8)}█{new(' ', 11)}{currentUpAlt[1]}   {(arrowOption is 1 ? "╞═●═╡" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}▀▄▄▀ █    ▄{new(' ', 28)}{boxSide} {currentUp[2]} {boxSide}{new(' ', 8)}█{new(' ', 11)}{currentUpAlt[2]}   {(arrowOption is 1 ? "╰───╯" : "     ")}");
-		sb.AppendLine($@"{new(' ', 89)}{boxBottom}{new(' ', 18)}{boxBottom}");
+		sb.AppendLine($@"{new(' ', 89)}{new(' ', 18)}");
+		sb.AppendLine(@$"{optionIndent}{blackAsciiText[0]}{new(' ', 28)}{new(' ', 8)}█{new(' ', 11)}   {(arrowOption is 1 ? "╭───╮" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{blackAsciiText[1]}{new(' ', 28)}{new(' ', 8)}█{new(' ', 11)}   {(arrowOption is 1 ? "╞═●═╡" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{blackAsciiText[2]}{new(' ', 28)}{new(' ', 8)}█{new(' ', 11)}   {(arrowOption is 1 ? "╰───╯" : "     ")}");
+		sb.AppendLine($@"{new(' ', 89)}{new(' ', 18)}");
 		sb.AppendLine();
-		sb.AppendLine($@"{new(' ', 89)}{boxTop}{new(' ', 18)}{boxTop}");
-		sb.AppendLine(@$"{optionIndent}▄▄▄   ▄▄  ▄   ▄ ▄   ▄  {new(' ', 16)}{boxSide} {currentDown[0]} {boxSide}{new(' ', 8)}█{new(' ', 11)}{currentDownAlt[0]}   {(arrowOption is 2 ? "╭───╮" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█  █ █  █ █ ▄ █ █▀▄ █ ▀{new(' ', 16)}{boxSide} {currentDown[1]} {boxSide}{new(' ', 8)}█{new(' ', 11)}{currentDownAlt[1]}   {(arrowOption is 2 ? "╞═●═╡" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█▄▄▀ ▀▄▄▀ █▀ ▀█ █  ▀█ ▄{new(' ', 16)}{boxSide} {currentDown[2]} {boxSide}{new(' ', 8)}█{new(' ', 11)}{currentDownAlt[2]}   {(arrowOption is 2 ? "╰───╯" : "     ")}");
-		sb.AppendLine($@"{new(' ', 89)}{boxBottom}{new(' ', 18)}{boxBottom}");
+		sb.AppendLine($@"{new(' ', 89)}{new(' ', 18)}");
+		sb.AppendLine(@$"{optionIndent}{whiteAsciiText[0]}{new(' ', 16)}{new(' ', 8)}█{new(' ', 11)}   {(arrowOption is 2 ? "╭───╮" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{whiteAsciiText[1]}{new(' ', 16)}{new(' ', 8)}█{new(' ', 11)}   {(arrowOption is 2 ? "╞═●═╡" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{whiteAsciiText[2]}{new(' ', 16)}{new(' ', 8)}█{new(' ', 11)}   {(arrowOption is 2 ? "╰───╯" : "     ")}");
+		sb.AppendLine($@"{new(' ', 89)}{new(' ', 18)}");
 		sb.AppendLine();
-		sb.AppendLine($@"{new(' ', 89)}{boxTop}{new(' ', 18)}{boxTop}");
-		sb.AppendLine(@$"{optionIndent}▄   ▄▄▄▄ ▄▄▄▄ ▄▄▄▄▄  {new(' ', 18)}{boxSide} {currentLeft[0]} {boxSide}{new(' ', 8)}█{new(' ', 11)}{currentLeftAlt[0]}   {(arrowOption is 3 ? "╭───╮" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█   █▄▄  █▄▄    █   ▀{new(' ', 18)}{boxSide} {currentLeft[1]} {boxSide}{new(' ', 8)}█{new(' ', 11)}{currentLeftAlt[1]}   {(arrowOption is 3 ? "╞═●═╡" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█▄▄ █▄▄▄ █      █   ▄{new(' ', 18)}{boxSide} {currentLeft[2]} {boxSide}{new(' ', 8)}█{new(' ', 11)}{currentLeftAlt[2]}   {(arrowOption is 3 ? "╰───╯" : "     ")}");
-		sb.AppendLine($@"{new(' ', 89)}{boxBottom}{new(' ', 18)}{boxBottom}");
+		sb.AppendLine($@"{new(' ', 89)}{new(' ', 18)}");
+		sb.AppendLine(@$"{optionIndent}{greenAsciiText[0]}{new(' ', 18)}{new(' ', 8)}█{new(' ', 11)}   {(arrowOption is 3 ? "╭───╮" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{greenAsciiText[1]}{new(' ', 18)}{new(' ', 8)}█{new(' ', 11)}   {(arrowOption is 3 ? "╞═●═╡" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{greenAsciiText[2]}{new(' ', 18)}{new(' ', 8)}█{new(' ', 11)}   {(arrowOption is 3 ? "╰───╯" : "     ")}");
+		sb.AppendLine($@"{new(' ', 89)}{new(' ', 18)}");
 		sb.AppendLine();
-		sb.AppendLine($@"{new(' ', 89)}{boxTop}{new(' ', 18)}{boxTop}");
-		sb.AppendLine(@$"{optionIndent}▄▄▄  ▄  ▄▄▄  ▄  ▄ ▄▄▄▄▄  {new(' ', 14)}{boxSide} {currentRight[0]} {boxSide}{new(' ', 8)}█{new(' ', 11)}{currentRightAlt[0]}   {(arrowOption is 4 ? "╭───╮" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█▄▄▀ █ █  ▄▄ █▄▄█   █   ▀{new(' ', 14)}{boxSide} {currentRight[1]} {boxSide}{new(' ', 8)}█{new(' ', 11)}{currentRightAlt[1]}   {(arrowOption is 4 ? "╞═●═╡" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█  █ █ ▀▄▄▄▀ █  █   █   ▄{new(' ', 14)}{boxSide} {currentRight[2]} {boxSide}{new(' ', 8)}█{new(' ', 11)}{currentRightAlt[2]}   {(arrowOption is 4 ? "╰───╯" : "     ")}");
-		sb.AppendLine($@"{new(' ', 89)}{boxBottom}{new(' ', 18)}{boxBottom}");
+		sb.AppendLine($@"{new(' ', 89)}{new(' ', 18)}");
+		sb.AppendLine(@$"{optionIndent}{redAsciiText[0]}{new(' ', 14)}{new(' ', 8)}█{new(' ', 11)}   {(arrowOption is 4 ? "╭───╮" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{redAsciiText[1]}{new(' ', 14)}{new(' ', 8)}█{new(' ', 11)}   {(arrowOption is 4 ? "╞═●═╡" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{redAsciiText[2]}{new(' ', 14)}{new(' ', 8)}█{new(' ', 11)}   {(arrowOption is 4 ? "╰───╯" : "     ")}");
+		sb.AppendLine($@"{new(' ', 89)}{new(' ', 18)}");
 		sb.AppendLine();
-		sb.AppendLine($@"{new(' ', 89)}{boxTop}{new(' ', 18)}{boxTop}");
-		sb.AppendLine(@$"{optionIndent} ▄▄   ▄▄▄ ▄▄▄▄▄ ▄  ▄▄  ▄   ▄  {new(' ', 9)}{boxSide} {currentInteract[0]} {boxSide}{new(' ', 8)}█{new(' ', 11)}{currentInteractAlt[0]}   {(arrowOption is 5 ? "╭───╮" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█▄▄█ █      █   █ █  █ █▀▄ █ ▀{new(' ', 9)}{boxSide} {currentInteract[1]} {boxSide}{new(' ', 8)}█{new(' ', 11)}{currentInteractAlt[1]}   {(arrowOption is 5 ? "╞═●═╡" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█  █ ▀▄▄▄   █   █ ▀▄▄▀ █  ▀█ ▄{new(' ', 9)}{boxSide} {currentInteract[2]} {boxSide}{new(' ', 8)}█{new(' ', 11)}{currentInteractAlt[2]}   {(arrowOption is 5 ? "╰───╯" : "     ")}");
-		sb.AppendLine($@"{new(' ', 89)}{boxBottom}{new(' ', 18)}{boxBottom}");
+		sb.AppendLine($@"{new(' ', 89)}{new(' ', 18)}");
+		sb.AppendLine(@$"{optionIndent}{blueAsciiText[0]}{new(' ', 9)}{new(' ', 8)}█{new(' ', 11)}   {(arrowOption is 5 ? "╭───╮" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{blueAsciiText[1]}{new(' ', 9)}{new(' ', 8)}█{new(' ', 11)}   {(arrowOption is 5 ? "╞═●═╡" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{blueAsciiText[2]}{new(' ', 9)}{new(' ', 8)}█{new(' ', 11)}   {(arrowOption is 5 ? "╰───╯" : "     ")}");
+		sb.AppendLine($@"{new(' ', 89)}{new(' ', 18)}");
 		sb.AppendLine();
-		sb.AppendLine(@$"{optionIndent}█▀▀▄  ▄▄   ▄▄▄ ▄  ▄   {(arrowOption is maxOption ? "╭───╮" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█■■█ █▄▄█ █    █■█    {(arrowOption is maxOption ? "╞═●═╡" : "     ")}");
-		sb.AppendLine(@$"{optionIndent}█▄▄▀ █  █ ▀▄▄▄ █  ▀▄  {(arrowOption is maxOption ? "╰───╯" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{returnToOptionsText[0]} {(arrowOption is maxOption ? "╭───╮" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{returnToOptionsText[1]} {(arrowOption is maxOption ? "╞═●═╡" : "     ")}");
+		sb.AppendLine(@$"{optionIndent}{returnToOptionsText[2]} {(arrowOption is maxOption ? "╰───╯" : "     ")}");
 
 		Console.SetCursorPosition(0, 0);
 		Console.WriteLine(sb);
@@ -75,12 +83,18 @@ public static class ConsoleColorSettingsScreen
 			case ConsoleKey.DownArrow or ConsoleKey.S: arrowOption = Math.Min(maxOption, arrowOption + 1); goto ReDraw;
 			case ConsoleKey.Enter or ConsoleKey.E:
 				switch (arrowOption)
-				{
-                                        // TODO: finish this implementation and experimentation 
-					case 1: Console.BackgroundColor = ConsoleColor.Blue; goto ReDraw;
-					case 2: goto ReDraw;
-					case 3: goto ReDraw;
-					case 4: goto ReDraw;
+				{ 
+					// TODO: Add the ability to choose what background and foreground you want to use individually
+					case 1: Console.BackgroundColor = ConsoleColor.Black;
+						Console.ForegroundColor = ConsoleColor.White; goto ReDraw;
+					case 2: Console.BackgroundColor = ConsoleColor.White;
+						Console.ForegroundColor = ConsoleColor.Black; goto ReDraw;
+					case 3: Console.BackgroundColor = ConsoleColor.Green;
+						Console.ForegroundColor = ConsoleColor.Black; goto ReDraw;
+					case 4: Console.BackgroundColor = ConsoleColor.Red;
+						Console.ForegroundColor = ConsoleColor.Black; goto ReDraw;
+					case 5: Console.BackgroundColor = ConsoleColor.Blue;
+						Console.ForegroundColor = ConsoleColor.Black; goto ReDraw;
 					case maxOption: break;
 				}
 				break;
