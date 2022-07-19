5f8772a06f98466620fb7e018a5302c2fddf40ae
HUNTERSCOUTI <39131351+HUNTERSCOUTI@users.noreply.github.com>
2022-07-19 04:02:01 +0200

    More Characters

---
+++ Projects/Console Monsters/Characters/CopyPaste.cs
@@ -0,0 +1,24 @@
+﻿namespace Console_Monsters.Characters;
+
+public class name : CharacterBase
+{
+	public name()
+	{
+		Sprite = IdleFront;
+
+		Dialogue = new string[]
+		{
+			"name says:" +
+			"I'm a copy paste file"
+		};
+	}
+
+	public override string? Name => "name";
+
+	public static readonly string IdleFront =
+		@"COPY" + '\n' +
+		@"COPY" + '\n' +
+		@"COPY" + '\n' +
+		@"COPY" + '\n' +
+		@"COPY";
+}
---
+++ Projects/Console Monsters/Characters/Samurai.cs
@@ -0,0 +1,24 @@
+﻿namespace Console_Monsters.Characters;
+
+public class Samurai : CharacterBase
+{
+	public Samurai()
+	{
+		Sprite = IdleFront;
+
+		Dialogue = new string[]
+		{
+			"Samurai says:" +
+			"I'm a copy paste file"
+		};
+	}
+
+	public override string? Name => "Samurai";
+
+	public static readonly string IdleFront =
+		@" /███\ " + '\n' +
+		@"/│'_'│\" + '\n' +
+		@"╭╰───╯╮" + '\n' +
+		@"│├─\─┤│" + '\n' +
+		@" │_|_│ ";
+}
