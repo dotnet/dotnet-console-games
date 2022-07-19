6b06e05843909e3f8c42e3344a85b2dd94ef9b16
HUNTERSCOUTI <39131351+HUNTERSCOUTI@users.noreply.github.com>
2022-07-19 04:02:01 +0200

    More Characters

---
+++ Projects/Console Monsters/Characters/LittleGirl.cs
@@ -0,0 +1,24 @@
+﻿namespace Console_Monsters.Characters;
+
+public class LittleGirl : CharacterBase
+{
+	public LittleGirl()
+	{
+		Sprite = IdleFront;
+
+		Dialogue = new string[]
+		{
+			"Little Girl says:" +
+			"I'm a copy paste file"
+		};
+	}
+
+	public override string? Name => "Little Girl";
+
+	public static readonly string IdleFront =
+		@"╭╭───╮╮" + '\n' +
+		@" │^_^│ " + '\n' +
+		@"╭┴───┴╮" + '\n' +
+		@"│├───┤│" + '\n' +
+		@" │_|_│ ";
+}
