2cbdb70730307ad2ab922990ac467fb61e459830
Zachary Patten <sevenix.zp@gmail.com>
2022-07-28 22:40:30 -0400

    #48

---
+++ Projects/Console Monsters/Monsters/MultiHeadedBird1.cs
@@ -0,0 +1,19 @@
+﻿namespace Console_Monsters.Monsters;
+
+public class MultiHeadedBird1 : MonsterBase
+{
+	public MultiHeadedBird1()
+	{
+		Name = "Two Headed Bird";
+	}
+
+	public override string[] Sprite => (
+		@" ╭───╮   ╭───╮" + "\n" +
+		@" │⁰v⁰│   │⁰v⁰│" + "\n" +
+		@" ╰──┬╯   ╰┬──╯" + "\n" +
+		@"    ╰╭───╮╯   " + "\n" +
+		@"     │   │    " + "\n" +
+		@"     ╰┬─┬╯    " + "\n" +
+		@"      │ │     " + "\n" +
+		@"      ┘ └     ").Split('\n');
+}
