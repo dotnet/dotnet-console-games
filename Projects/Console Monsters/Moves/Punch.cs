e64332e5ea7d070244ee765262cbafd4408ac919
Zilas Jørgensen <zila0082@elev.techcollege.dk>
2022-08-09 12:25:02 +0200

    Base Damage Fix

--- Projects/Console Monsters/Moves/Punch.cs
+++ Projects/Console Monsters/Moves/Punch.cs
@@ -6,7 +6,7 @@ public class Punch : MoveBase
 	{
 		Name = "Punch";
 		BaseDamage = 40;
-		FinalDamage = (((2 * AttackingMonster!.Level / 5 + 2) * BaseDamge * AttackingMonster.AttackStat / DefendingMonster!.DefenseStat) / 50 + 2) * BattleRandom.Next(85, 101) / 100;
+		FinalDamage = (((2 * AttackingMonster!.Level / 5 + 2) * BaseDamage * AttackingMonster.AttackStat / DefendingMonster!.DefenseStat) / 50 + 2) * BattleRandom.Next(85, 101) / 100;
 		EnergyTaken = 10; // Temp
 	}
 }
