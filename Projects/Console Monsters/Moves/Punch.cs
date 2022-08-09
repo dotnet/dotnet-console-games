ea0346890816683e87c6a2186a65d417e6fe8c47
Zilas Jørgensen <zila0082@elev.techcollege.dk>
2022-08-09 08:02:49 +0200

    Spelling Mistake

--- Projects/Console Monsters/Moves/Punch.cs
+++ Projects/Console Monsters/Moves/Punch.cs
@@ -5,7 +5,7 @@ public class Punch : MoveBase
 	public Punch()
 	{
 		Name = "Punch";
-		BaseDamge = 40;
+		BaseDamage = 40;
 		FinalDamage = (((2 * AttackingMonster!.Level / 5 + 2) * BaseDamge * AttackingMonster.AttackStat / DefendingMonster!.DefenseStat) / 50 + 2) * BattleRandom.Next(85, 101) / 100;
 		EnergyTaken = 10; // Temp
 	}
