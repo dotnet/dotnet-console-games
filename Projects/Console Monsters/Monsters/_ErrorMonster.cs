cce359aea83744d77b721d9887e00f726be6d6c0
Zilas Jørgensen <Zilas2004@gmail.com>
2022-09-21 10:34:14 +0200

    Error monster update

---
+++ Projects/Console Monsters/Monsters/_ErrorMonster.cs
@@ -0,0 +1,16 @@
+﻿namespace Console_Monsters.Monsters;
+
+public class _ErrorMonster : MonsterBase
+{
+	public _ErrorMonster()
+	{
+		Name = "No Monster";
+	}
+
+	public override string[] Sprite => (
+		"╔═════╗" + "\n" +
+		"║ERROR║" + "\n" +
+		"║ERROR║" + "\n" +
+		"║ERROR║" + "\n" +
+		"╚═════╝").Split('\n');
+}
