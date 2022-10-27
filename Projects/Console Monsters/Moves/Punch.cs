5cc3fcb89a8d5c2f87f9f56d0b738fc4cc9a8893
HUNTERSCOUTI <39131351+HUNTERSCOUTI@users.noreply.github.com>
2022-10-27 17:10:20 +0200

    BaseDamage => Power

--- Projects/Console Monsters/Moves/Punch.cs
+++ Projects/Console Monsters/Moves/Punch.cs
@@ -5,8 +5,10 @@ public class Punch : MoveBase
 	public Punch()
 	{
 		Name = "Punch";
-		BaseDamage = 40;
-		FinalDamage = (((2 * AttackingMonster!.Level / 5 + 2) * BaseDamage * AttackingMonster.AttackStat / DefendingMonster!.DefenseStat) / 50 + 2) * BattleRandom.Next(85, 101) / 100;
-		EnergyTaken = 10; // Temp
+		Power = 40;
+		FinalDamage = (((2 * AttackingMonster!.Level / 5 + 2) 
+			* Power * AttackingMonster.AttackStat / DefendingMonster!.DefenseStat) / 50 + 2) 
+			* BattleRandom.Next(85, 101) / 100;
+		EnergyTaken = 10;
 	}
 }
--- Projects/Console Monsters/Moves/Scratch.cs
+++ Projects/Console Monsters/Moves/Scratch.cs
@@ -5,8 +5,8 @@ public class Scratch : MoveBase
 	public Scratch()
 	{
 		Name = "Scratch";
-		BaseDamage = 40;
-		FinalDamage = (((2 * AttackingMonster!.Level / 5 + 2) * BaseDamage * AttackingMonster.AttackStat / DefendingMonster!.DefenseStat) / 50 + 2) * BattleRandom.Next(85, 101) / 100;
+		Power = 40;
+		FinalDamage = (((2 * AttackingMonster!.Level / 5 + 2) * Power * AttackingMonster.AttackStat / DefendingMonster!.DefenseStat) / 50 + 2) * BattleRandom.Next(85, 101) / 100;
 		EnergyTaken = 10; // Temp
 	}
 }
