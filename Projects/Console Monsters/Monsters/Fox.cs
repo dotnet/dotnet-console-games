c263b6b20bca7fa875f1f89b35329d78ceff0f24
HUNTERSCOUTI <39131351+HUNTERSCOUTI@users.noreply.github.com>
2022-10-27 23:38:37 +0200

    Fox Stats added

--- Projects/Console Monsters/Monsters/Fox.cs
+++ Projects/Console Monsters/Monsters/Fox.cs
@@ -1,10 +1,27 @@
-﻿namespace Console_Monsters.Monsters;
+﻿using Console_Monsters.Moves;
+
+namespace Console_Monsters.Monsters;
 
 internal class Fox : MonsterBase
 {
 	public Fox()
 	{
 		Name = "Fox";
+		Level = 5;
+		MaximumHP = 20;
+		MaximumEnergy = 50;
+		CurrentHP = MaximumHP;
+		CurrentEnergy = MaximumEnergy;
+		Evolution = 1;
+		AttackStat = 10;
+		SpeedStat = 10;
+		DefenseStat = 10;
+		MoveSet = new List<MoveBase>
+		{
+			new Punch(),
+			new Tackle(),
+			new Scratch()
+		};
 	}
 
 	public override string[] Sprite => (
