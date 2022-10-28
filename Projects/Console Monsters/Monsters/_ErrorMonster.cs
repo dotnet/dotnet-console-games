938f72fe7168a58d49ce14c89dbc5d29176ffe8e
zilas jørgensen <zilas2004@gmail.com>
2022-10-28 10:59:45 +0200

    Errormonster Values for testing

--- Projects/Console Monsters/Monsters/_ErrorMonster.cs
+++ Projects/Console Monsters/Monsters/_ErrorMonster.cs
@@ -5,6 +5,9 @@ public class _ErrorMonster : MonsterBase
 	public _ErrorMonster()
 	{
 		Name = "No Monster";
+		AttackStat = 0;
+		DefenseStat = 0;
+		SpeedStat = 0;
 	}
 
 	public override string[] Sprite => (
