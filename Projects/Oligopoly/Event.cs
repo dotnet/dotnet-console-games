cf45168058a239e4060aa69441de64446e63ba8e
ZacharyPatten <sevenix.zp@gmail.com>
2023-05-10 19:48:57 -0500

    clean up and "more company info" bug fix

--- Projects/Oligopoly/Event.cs
+++ Projects/Oligopoly/Event.cs
@@ -5,7 +5,7 @@ namespace Oligopoly;
 public class Event
 {
 	public int Effect { get; set; }
-	public string Target { get; set; }
+	public string CompanyName { get; set; }
 	public string Type { get; set; }
 	public string Title { get; set; }
 	public string Content { get; set; }
